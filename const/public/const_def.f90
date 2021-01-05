5d70244ea320fac6e697e2642b8963eb30a827fc
Robert Farmer <r.j.farmer@uva.nl>
2021-01-05 19:14:34 +0100

    Update weinberg angle to be consistent with CODATA 2018
    It leads to an change in the ~third sig fig for the neutrino rates

--- const/public/const_def.f90
+++ const/public/const_def.f90
@@ -106,7 +106,7 @@
 
       real(dp), parameter :: sige = 6.6524587321d-25 ! Thomson cross section (cm^2)
 
-      real(dp), parameter :: weinberg_theta  = 0.2319d0 ! sin**2(theta_weinberg) = 0.2319 plus/minus 0.00005 (1996)
+      real(dp), parameter :: weinberg_theta  = 0.22290d0 ! sin**2(theta_weinberg)
       real(dp), parameter :: num_neu_fam = 3.0d0 ! number of neutrino flavors = 3.02 plus/minus 0.005 (1998)
 
 ! the following quantities are not exact
