56ab5313c77a14a68d1ba6332fa3ab14d53f7a3e
Adam Jermyn <adamjermyn@gmail.com>
2021-01-25 19:07:17 -0500

    [ci skip] Made auto_diff array types work again.

--- auto_diff/public/auto_diff.f90
+++ auto_diff/public/auto_diff.f90
@@ -1,9 +1,4 @@
 module auto_diff
    use support_functions
-   use auto_diff_real_2var_order2_module
-   use auto_diff_real_4var_order1_module
-   use auto_diff_real_1var_order1_module
-   use auto_diff_real_2var_order1_module
-   use auto_diff_real_2var_order3_module
-   use auto_diff_real_18var_order1_module
+   use auto_diff_real_2var_order3_array_module
 end module auto_diff
