7470efe2053a1ea5c8e37f0e0a3c4c5749c17eb4
Rich Townsend <townsend@astro.wisc.edu>
2021-03-08 20:05:07 -0600

    Changes to pulse_gsm to work around gfortran PR 99477

--- star/private/pulse_gsm.f90
+++ star/private/pulse_gsm.f90
@@ -68,51 +68,29 @@ contains
     
     call hi%write_attr('version', GSM_MODEL_VERSION)
 
-    associate ( &
-         r => point_data(1,:), &
-         m => point_data(2,:), &
-         L => point_data(3,:), &
-         P => point_data(4,:), &
-         T => point_data(5,:), &
-         rho => point_data(6,:), &
-         nabla => point_data(7,:), &
-         N2 => point_data(8,:), &
-         Gamma_1 => point_data(9,:), &
-         nabla_ad => point_data(10,:), &
-         delta => point_data(11,:), &
-         kap => point_data(12,:), &
-         kap_T => point_data(13,:), &
-         kap_rho => point_data(14,:), &
-         eps => point_data(15,:), &
-         eps_T => point_data(16,:), &
-         eps_rho => point_data(17,:), &
-         omega => point_data(18,:))
+    call hi%write_dset('r', point_data(1,:))
+    call hi%write_dset('M_r', point_data(2,:))
+    call hi%write_dset('L_r', point_data(3,:))
 
-      call hi%write_dset('r', r)
-      call hi%write_dset('M_r', m)
-      call hi%write_dset('L_r', L)
-
-      call hi%write_dset('P', P)
-      call hi%write_dset('rho', rho)
-      call hi%write_dset('T', T)
+    call hi%write_dset('P', point_data(4,:))
+    call hi%write_dset('T', point_data(5,:))
+    call hi%write_dset('rho', point_data(6,:))
       
-      call hi%write_dset('N2', N2)
-      call hi%write_dset('Gamma_1', Gamma_1)
-      call hi%write_dset('nabla_ad', nabla_ad)
-      call hi%write_dset('delta', delta)
-      call hi%write_dset('nabla', nabla)
+    call hi%write_dset('nabla', point_data(7,:))
+    call hi%write_dset('N2', point_data(8,:))
+    call hi%write_dset('Gamma_1', point_data(9,:))
+    call hi%write_dset('nabla_ad', point_data(10,:))
+    call hi%write_dset('delta', point_data(11,:))
 
-      call hi%write_dset('kap', kap)
-      call hi%write_dset('kap_kap_T', kap_T)
-      call hi%write_dset('kap_kap_rho', kap_rho)
+    call hi%write_dset('kap', point_data(12,:))
+    call hi%write_dset('kap_kap_T', point_data(13,:))
+    call hi%write_dset('kap_kap_rho', point_data(14,:))
 
-      call hi%write_dset('eps', eps)
-      call hi%write_dset('eps_eps_T', eps_T)
-      call hi%write_dset('eps_eps_rho', eps_rho)
+    call hi%write_dset('eps', point_data(15,:))
+    call hi%write_dset('eps_eps_T', point_data(16,:))
+    call hi%write_dset('eps_eps_rho', point_data(17,:))
 
-      call hi%write_dset('Omega_rot', omega)
-
-    end associate
+    call hi%write_dset('Omega_rot', point_data(18,:))
 
     ! Close the file
 
