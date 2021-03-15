18e8753181f92d2c4d1b797a122a74e416ca45f5
Josiah Schwab <jschwab@gmail.com>
2021-03-15 09:44:11 -0700

    Miscellaneous tweaks made while testing net partials from star
    * Remove unused variables
    * Set net_test_partials_iother to i_var_sink
    * Import test_partials vars in net_derivs_support

--- net/private/net_derivs_support.f90
+++ net/private/net_derivs_support.f90
@@ -25,7 +25,9 @@
 ! ***********************************************************************
 
       module net_derivs_support
-      use net_def, only: Net_General_Info, Net_Info
+      use net_def, only: Net_General_Info, Net_Info, net_test_partials, &
+         net_test_partials_val, net_test_partials_dval_dx, net_test_partials_i, &
+         net_test_partials_iother
       use const_def
       use chem_def
       use rates_def
