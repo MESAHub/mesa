907ce2b158f6d024fd9b410a1ce447fb0fa4f6fa
Adam Jermyn <adamjermyn@gmail.com>
2021-04-07 13:42:28 -0400

    Added new auto_diff type for TDC experiments. [ci skip]

--- auto_diff/public/auto_diff.f90
+++ auto_diff/public/auto_diff.f90
@@ -1,8 +1,8 @@
 module auto_diff
    use support_functions
-   !use support_functions_quad
    use auto_diff_real_2var_order2_module
    use auto_diff_real_4var_order1_module
+   use auto_diff_real_tdc_module
    use auto_diff_real_1var_order1_module
    use auto_diff_real_2var_order1_module
    use auto_diff_real_star_order1_module
