66e0f78ef32f799e01536bc25c8b1b0d796c3e04
Josiah Schwab <jschwab@gmail.com>
2021-05-09 15:58:29 -0700

    Simplify energy equation switching (#238)
    Remove some energy equation options whose complexity is currently unneeded.
    
    * Remove per-cell energy equation controls max_eta_for_dedt_form_of_energy_eqn and max_gamma_for_dedt_form_of_energy_eqn
    
    * Remove control steps_before_always_use_dedt_form_of_energy_eqn

--- star/private/ctrls_io.f90
+++ star/private/ctrls_io.f90
@@ -339,10 +339,10 @@
     ! hydro parameters
     opacity_factor, opacity_max, min_logT_for_opacity_factor_off, min_logT_for_opacity_factor_on, &
     max_logT_for_opacity_factor_on, max_logT_for_opacity_factor_off, &
-    non_nuc_neu_factor, always_use_dedt_form_of_energy_eqn, steps_before_always_use_dedt_form_of_energy_eqn, &
+    non_nuc_neu_factor, always_use_dedt_form_of_energy_eqn, &
     use_dedt_form_of_energy_eqn, use_time_centered_eps_grav, &
-    use_mass_corrections, use_gravity_rotation_correction, eps_grav_factor, eps_mdot_factor, max_gamma_for_dedt_form_of_energy_eqn, &
-    include_composition_in_eps_grav, max_eta_for_dedt_form_of_energy_eqn, no_dedt_form_during_relax, &
+    use_mass_corrections, use_gravity_rotation_correction, eps_grav_factor, eps_mdot_factor, &
+    include_composition_in_eps_grav, no_dedt_form_during_relax, &
     max_abs_rel_change_surf_lnS, always_use_eps_grav_form_of_energy_eqn, &
     max_num_surf_revisions, Gamma_lnS_eps_grav_full_off, Gamma_lnS_eps_grav_full_on, &
     use_dPrad_dm_form_of_T_gradient_eqn, use_gradT_actual_vs_gradT_MLT_for_T_gradient_eqn, dedt_eqn_r_scale, &
@@ -1828,10 +1828,7 @@
  s% non_nuc_neu_factor = non_nuc_neu_factor
  s% use_dedt_form_of_energy_eqn = use_dedt_form_of_energy_eqn
  s% always_use_dedt_form_of_energy_eqn = always_use_dedt_form_of_energy_eqn
- s% steps_before_always_use_dedt_form_of_energy_eqn = steps_before_always_use_dedt_form_of_energy_eqn
  s% use_time_centered_eps_grav = use_time_centered_eps_grav
- s% max_eta_for_dedt_form_of_energy_eqn = max_eta_for_dedt_form_of_energy_eqn
- s% max_gamma_for_dedt_form_of_energy_eqn = max_gamma_for_dedt_form_of_energy_eqn
  s% no_dedt_form_during_relax = no_dedt_form_during_relax
  s% always_use_eps_grav_form_of_energy_eqn = always_use_eps_grav_form_of_energy_eqn
  s% dedt_eqn_r_scale = dedt_eqn_r_scale
@@ -3497,10 +3494,7 @@
  non_nuc_neu_factor = s% non_nuc_neu_factor
  use_dedt_form_of_energy_eqn = s% use_dedt_form_of_energy_eqn
  always_use_dedt_form_of_energy_eqn = s% always_use_dedt_form_of_energy_eqn
- steps_before_always_use_dedt_form_of_energy_eqn = s% steps_before_always_use_dedt_form_of_energy_eqn
  use_time_centered_eps_grav = s% use_time_centered_eps_grav
- max_eta_for_dedt_form_of_energy_eqn = s% max_eta_for_dedt_form_of_energy_eqn
- max_gamma_for_dedt_form_of_energy_eqn = s% max_gamma_for_dedt_form_of_energy_eqn
  no_dedt_form_during_relax = s% no_dedt_form_during_relax
  always_use_eps_grav_form_of_energy_eqn = s% always_use_eps_grav_form_of_energy_eqn
  dedt_eqn_r_scale = s% dedt_eqn_r_scale
