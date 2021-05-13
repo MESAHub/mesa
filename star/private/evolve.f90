d1b7e30c1af55dc699411e55c79a66cc918d531a
Adam Jermyn <adamjermyn@gmail.com>
2021-05-13 15:30:15 -0400

    Reenabled do_report at end of part 2.

--- star/private/evolve.f90
+++ star/private/evolve.f90
@@ -748,8 +748,8 @@
 
          s% total_angular_momentum = total_angular_momentum(s)
          ! cannot call do_report yet since many things it needs are not yet set.
-         !call do_report(s, ierr)
-         !if (failed('do_report')) return
+         call do_report(s, ierr)
+         if (failed('do_report')) return
          call set_phase_of_evolution(s)
             
          call system_clock(time0,clock_rate)
