0c3b58e954c5fc1d9f5810ce44d06a962429ec6e
Adam Jermyn <adamjermyn@gmail.com>
2021-05-13 15:30:26 -0400

    [ci skip] Comment.

--- star/private/evolve.f90
+++ star/private/evolve.f90
@@ -747,7 +747,6 @@
          call set_power_info(s)
 
          s% total_angular_momentum = total_angular_momentum(s)
-         ! cannot call do_report yet since many things it needs are not yet set.
          call do_report(s, ierr)
          if (failed('do_report')) return
          call set_phase_of_evolution(s)
