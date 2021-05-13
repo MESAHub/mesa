fb75211fbd239c5d10871749d20b768c69f2708d
Adam Jermyn <adamjermyn@gmail.com>
2021-05-13 17:14:24 -0400

    [ci skip] Removed more state.

--- star/private/struct_burn_mix.f90
+++ star/private/struct_burn_mix.f90
@@ -334,9 +334,6 @@
             s% latent_ddlnRho_start(k) = s% latent_ddlnRho(k)
             s% eps_nuc_start(k) = s% eps_nuc(k)
             s% opacity_start(k) = s% opacity(k)
-            s% mlt_mixing_type_start(k) = s% mlt_mixing_type(k)
-            s% mlt_D_start(k) = s% mlt_D(k)
-            s% mlt_Gamma_start(k) = s% mlt_Gamma(k)
          end do
          
          if (s% using_RSP2) then
