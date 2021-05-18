5c6280d6b057fad5b09511c7d817147d41a8d775
bill-paxton <paxton@kitp.ucsb.edu>
2021-05-18 11:07:32 -0700

    add rsp2 test cases (#253)
    Now have same set of test cases as for rsp

--- star/dev_cases_compare_pulses/dev_compare_Cepheid_RSP2_TDC/src/run_star_extras.f90
+++ star/dev_cases_compare_pulses/dev_compare_Cepheid_RSP2_TDC/src/run_star_extras.f90
@@ -57,12 +57,36 @@
          integer :: ierr
          real(dp) :: target_period, rel_run_E_err, time_ended
          type (star_info), pointer :: s, s_other
-         integer :: id_other
+         integer :: id_other, k
          include 'formats'
          ierr = 0
          call star_ptr(id, s, ierr)
          if (ierr /= 0) return
          extras_finish_step = keep_going
+         if (s% id == 2 .and. s% x_logical_ctrl(1)) then
+            write(*,2) 'set star2 xh = star1 xh to resynchronize', s% model_number
+            id_other = 1
+            call star_ptr(id_other, s_other, ierr)
+            if (ierr /= 0) return
+            ! sanity check before start copying
+            if (s% model_number /= s_other% model_number .or. &
+               s% nz /= s_other% nz .or. &
+               s% R_center /= s_other% R_center .or. &
+               s% L_center /= s_other% L_center .or. &
+               s% m_center /= s_other% m_center .or. &
+               s% xmstar /= s_other% xmstar) then
+              write(*,2) 'something does not match', s% model_number
+              stop 'extras_finish_step'
+            end if
+            do k=1,s% nz
+               s% xh(s% i_lum,k) = s_other% L(k)
+               s% xh(s% i_v,k) = s_other% v(k)
+               s% mlt_vc(k) = s_other% w(k)*sqrt_2_div_3
+               s% xh(s% i_lnR,k) = s_other% lnR(k)
+               s% xh(s% i_lnT,k) = s_other% lnT(k)
+               s% xh(s% i_lnd,k) = s_other% lnd(k)
+            end do
+         end if
       end function extras_finish_step
       
       
@@ -97,11 +121,6 @@
          
          if (id == 1 .and. .not. s% RSP2_flag) then
             write(*,*) 'star id==1, but not RSP2_flag'
-            stop 'extras_startup'
-         end if
-         
-         if (id == 2 .and. s% RSP2_flag) then
-            write(*,*) 'star id==2, but RSP2_flag'
             stop 'extras_startup'
          end if
 
@@ -186,7 +205,7 @@
          ierr = 0
          call star_ptr(id, s, ierr)
          if (ierr /= 0) return
-         how_many_extra_profile_columns = 27
+         how_many_extra_profile_columns = 38
       end function how_many_extra_profile_columns
       
       
@@ -217,36 +236,47 @@
          
          i=1
          names(i) = 'v_R'; i=i+1
+         names(i) = 'v_diff'; i=i+1
          names(i) = 'v_drel'; i=i+1
          
          names(i) = 'Y_face_R'; i=i+1
+         names(i) = 'Y_diff'; i=i+1
          names(i) = 'Y_drel'; i=i+1
          
-         names(i) = 'w_R'; i=i+1
-         names(i) = 'w_drel'; i=i+1
+         names(i) = 'vc_R'; i=i+1
+         names(i) = 'vc_diff'; i=i+1
+         names(i) = 'vc_drel'; i=i+1
          
          names(i) = 'Lc_div_L_R'; i=i+1
+         names(i) = 'Lc_diff'; i=i+1
          names(i) = 'Lc_drel'; i=i+1
          
          names(i) = 'COUPL_R'; i=i+1
+         names(i) = 'CPL_diff'; i=i+1
          names(i) = 'CPL_drel'; i=i+1
          
          names(i) = 'SRC_R'; i=i+1
+         names(i) = 'SRC_diff'; i=i+1
          names(i) = 'SRC_drel'; i=i+1
          
          names(i) = 'DAMP_R'; i=i+1
+         names(i) = 'DAMP_diff'; i=i+1
          names(i) = 'DAMP_drel'; i=i+1
          
          names(i) = 'DAMPR_R'; i=i+1
+         names(i) = 'DAMPR_diff'; i=i+1
          names(i) = 'DAMPR_drel'; i=i+1
          
          names(i) = 'Eq_R'; i=i+1
+         names(i) = 'Eq_diff'; i=i+1
          names(i) = 'Eq_drel'; i=i+1
          
          names(i) = 'Uq_R'; i=i+1
+         names(i) = 'Uq_diff'; i=i+1
          names(i) = 'Uq_drel'; i=i+1
          
          names(i) = 'Pvsc_R'; i=i+1
+         names(i) = 'Pvsc_diff'; i=i+1
          names(i) = 'Pvsc_drel'; i=i+1
 
          names(i) = 'logR_R'; i=i+1
@@ -264,44 +294,59 @@
             
                i = 1
                vals(k,i) = s_other% v(k)*1d-5; i=i+1
+               vals(k,i) = s_other% v(k) - s% v(k); i=i+1
                vals(k,i) = rel_diff(s_other% v(k), s% v(k)); i=i+1
          
                vals(k,i) = s_other% Y_face(k); i=i+1
+               vals(k,i) = s_other% Y_face(k) - s% Y_face(k); i=i+1
                vals(k,i) = rel_diff(s_other% Y_face(k), s% Y_face(k)); i=i+1
                
                if (s_other% RSP2_flag) then
-                  val = s_other% w(k)
-               else if (s_other% RSP_flag) then
-                  val = s_other% RSP_w(k)
+                  val = s_other% w(k)*sqrt_2_div_3
                else
                   val = 0d0
                end if
                vals(k,i) = val; i=i+1
-               vals(k,i) = rel_diff(val, s% w(k)); i=i+1
+               vals(k,i) = val - s% mlt_vc(k); i=i+1
+               if (s% doing_first_model_of_run .and. s% x_logical_ctrl(2) .and. &
+                     abs(vals(k,i-1)) > 1d0 .and. &
+                     max(1d0,abs(val),abs(s% mlt_vc(k))) > 1d2*min(abs(val),abs(s% mlt_vc(k)))) then
+                  write(*,3) 'vc diff', k, s% model_number, vals(k,i-1), val, s% mlt_vc(k)
+                  stop 'data_for_extra_profile_columns'
+               end if
+               vals(k,i) = rel_diff(val, s% mlt_vc(k)); i=i+1
                
                val = s_other% Lc(k)/s_other% L(k)
                vals(k,i) = val; i=i+1
+               vals(k,i) = val - s% Lc(k)/s% L(k); i=i+1
                vals(k,i) = rel_diff(val, s% Lc(k)/s% L(k)); i=i+1
          
                vals(k,i) = s_other% COUPL(k); i=i+1
+               vals(k,i) = s_other% COUPL(k) - s% COUPL(k); i=i+1
                vals(k,i) = rel_diff(s_other% COUPL(k), s% COUPL(k)); i=i+1
          
                vals(k,i) = s_other% SOURCE(k); i=i+1
+               vals(k,i) = s_other% SOURCE(k) - s% SOURCE(k); i=i+1
                vals(k,i) = rel_diff(s_other% SOURCE(k), s% SOURCE(k)); i=i+1
          
                vals(k,i) = s_other% DAMP(k); i=i+1
+               vals(k,i) = s_other% DAMP(k) - s% DAMP(k); i=i+1
                vals(k,i) = rel_diff(s_other% DAMP(k), s% DAMP(k)); i=i+1
          
                vals(k,i) = s_other% DAMPR(k); i=i+1
+               vals(k,i) = s_other% DAMPR(k) - s% DAMPR(k); i=i+1
                vals(k,i) = rel_diff(s_other% DAMPR(k), s% DAMPR(k)); i=i+1
          
                vals(k,i) = s_other% Eq(k); i=i+1
+               vals(k,i) = s_other% Eq(k) - s% Eq(k); i=i+1
                vals(k,i) = rel_diff(s_other% Eq(k), s% Eq(k)); i=i+1
          
                vals(k,i) = s_other% Uq(k); i=i+1
+               vals(k,i) = s_other% Uq(k) - s% Uq(k); i=i+1
                vals(k,i) = rel_diff(s_other% Uq(k), s% Uq(k)); i=i+1
          
                vals(k,i) = s_other% Pvsc(k); i=i+1
+               vals(k,i) = s_other% Pvsc(k) - s% Pvsc(k); i=i+1
                vals(k,i) = rel_diff(s_other% Pvsc(k), s% Pvsc(k)); i=i+1
 
                vals(k,i) = safe_log10(s_other% r(k)/Rsun); i=i+1
@@ -315,7 +360,7 @@
          
          contains
                
-         real(dp) function rel_diff(b, a, atol, rtol) result(d)
+         real(dp) function rel_diff(a, b, atol, rtol) result(d)
             real(dp), intent(in) :: a, b
             real(dp), intent(in), optional :: atol, rtol
             real(dp) :: atl, rtl
