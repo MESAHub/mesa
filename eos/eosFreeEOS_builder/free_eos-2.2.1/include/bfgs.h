ea65f60725ba09262eefda94edd347075bb9452d
Aaron Dotter <aaron.dotter@gmail.com>
2021-06-13 10:08:54 -0400

    clean up of FreeEOS table install directory

--- eos/eosFreeEOS_builder/free_eos-2.2.1/include/bfgs.h
+++
@@ -1,6 +0,0 @@
-      integer*4 nmax_bfgs
-      parameter(nmax_bfgs=300)
-      logical fletcher_estimate
-      real*8 alpha_previous, deltaf_previous
-      common/block_bfgs/alpha_previous, deltaf_previous,
-     &  fletcher_estimate
--- eos/eosFreeEOS_builder/free_eos-2.2.1/include/quad4block.h
+++
@@ -1,2 +0,0 @@
-      real*8 akindex, eta, beta, betahalf
-      common/linkquad4b/ akindex,eta,beta,betahalf
