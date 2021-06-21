7091e3dcb88c594b3886fe3c6f7d3b92282b3986
Robert Farmer <r.j.farmer@uva.nl>
2021-06-21 12:10:12 +0200

    Add linter for checking test suite owners
    Update CODEOWNERS with newest test_suite names

--- linters/README.rst
+++ linters/README.rst
@@ -47,4 +47,9 @@ Provides only a count per file of the number of issues found::
 
   python3 mesa-linter.py -s *.f90
 
+chek_test_suite_onwers.py
+-------------------------
 
+This script checks that each test suite case is listed in CODEOWNERS and
+checks whether each test case has 0 owners (very bad) or just 1 owner
+(less bad but should be fixed).
---
+++ linters/check_test_suite_owners.py
@@ -0,0 +1,100 @@
+import os
+import glob
+
+# Checks whether a test case has a owner and warns for test cases with only one owner
+
+MESA_DIR = os.environ['MESA_DIR']
+
+CODEOWNERS = os.path.join(MESA_DIR,'CODEOWNERS')
+
+STAR = os.path.join(MESA_DIR,'star','test_suite')
+BINARY = os.path.join(MESA_DIR,'binary','test_suite')
+ASTERO = os.path.join(MESA_DIR,'astero','test_suite')
+
+
+def get_test_cases(folder):
+    # Get test cases        
+    x = list(glob.glob(os.path.join(folder,'*/')))
+    return set([i.removeprefix(MESA_DIR).removesuffix('/').removeprefix('/') for i in x])
+
+
+def parse_codeowners(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    result = {
+        'star': {},
+        'binary': {},
+        'astero': {},
+        'module': {},
+    }
+    for line in lines:
+        if line.startswith('#') or len(line.strip())==0:
+            continue
+        
+        case = line.split()
+
+        if case[0].startswith('star/test_suite'):
+            result['star'][case[0]] = case[1:]
+        elif case[0].startswith('binary/test_suite'):
+            result['binary'][case[0]] = case[1:]
+        elif case[0].startswith('astero/test_suite'):
+            result['astero'][case[0]] = case[1:]
+        else:
+            result['module'][case[0]] = case[1:]
+
+    return result
+
+
+def print_section(header):
+    """Display output section header"""
+    print(f"\n\n*** {header} ***\n")
+
+def print_options(options):
+    """Print a set of options"""
+    for o in sorted(options):
+        print(f"   {o}")
+
+
+# Things with not in CODEOWNERS file
+def not_listed(cases, code):
+    listed = set(code.keys())
+    result = cases - listed
+    if len(result):
+        print_section('Test cases not in CODEOWNERS')
+        print_options(result)
+
+# Check number of owners
+def check_owners(code, num=0):
+    result=[]
+    for key,value in code:
+        if len(value)==num:
+            result.append(key)
+
+    if len(result):
+        print_section(f'Test cases with only {num} CODEOWNERS')
+        print_options(result)
+
+
+
+star_cases = get_test_cases(STAR)
+binary_cases = get_test_cases(BINARY)
+astero_cases = get_test_cases(ASTERO)
+
+codeowner = parse_codeowners(CODEOWNERS)
+
+
+
+not_listed(star_cases,codeowner['star'])
+not_listed(binary_cases,codeowner['binary'])
+not_listed(astero_cases,codeowner['astero'])
+
+
+check_owners(star_cases,codeowner['star'],0)
+check_owners(binary_cases,codeowner['binary'],0)
+check_owners(astero_cases,codeowner['astero'],0)
+
+
+check_owners(star_cases,codeowner['star'],1)
+check_owners(binary_cases,codeowner['binary'],1)
+check_owners(astero_cases,codeowner['astero'],1)
