f0fcc7e304a995c5cb604fc077f6351f11050a1a
Adam Jermyn <adamjermyn@gmail.com>
2021-06-28 13:46:33 -0400

    [ci optional] If you don’t have MLT_VC then it’s definitely okay to set it (and indeed we must).

--- star/private/read_model.f90
+++ star/private/read_model.f90
@@ -152,6 +152,10 @@
                return
             end if
          end if
+
+         if (.not. s% have_mlt_vc) then
+            s% okay_to_set_mlt_vc = .true.
+         end if
          
          s% doing_finish_load_model = .true.  
          call set_vars(s, s% dt, ierr)
