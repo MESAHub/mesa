05fea84731be89dc34e3956a2b46abd36a273e1c
Rob Farmer <robert.j.farmer37@gmail.com>
2021-12-01 12:24:28 +0100

    Release: r21.12.1-rc1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,8 @@ sys.path.insert(0, os.path.abspath('./_ext/'))
 project = 'MESA'
 copyright = '2021, The MESA Team'
 author = 'The MESA Team'
-release = 'main'
-version = 'main'
+release = 'r21.12.1-rc1'
+version = 'r21.12.1-rc1'
 
 
 # -- General configuration ---------------------------------------------------
