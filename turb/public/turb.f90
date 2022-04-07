e5fc7c28c1fe819ae693e0f7db6db839c5d4a613
Adam Jermyn <adamjermyn@gmail.com>
2022-04-07 10:58:24 -0400

    Turn TDC on by default. (#388)
    * [ci optional] Turned TDC on by default.
    
    * [ci optional] Disabled TDC in 1M_thermohaline and wd_stable_h_burn.
    
    * Increased max_model_number in 1M_thermohaline and turned on TDC.
    
    * [ci optional] Incorporated gradr_factor into TDC.
    
    * Fixed build test_output
    
    * Docs updates per Warrick
    
    * [ci skip] Set 1M to use Cox
    
    * Fixed turb test_output [ci optional]
    
    * [ci optional] Moved two tests back to MLT/Cox
    
    * [ci optional] Moved make_o_ne_wd to MLT.

--- turb/public/turb.f90
+++ turb/public/turb.f90
@@ -72,7 +72,7 @@ module turb
    !! @param scale The scale for computing residuals to the luminosity equation (erg/s).
    !! @param chiT dlnP/dlnT|rho
    !! @param chiRho dlnP/dlnRho|T
-   !! @param L Luminosity across a face (erg/s).
+   !! @param gradr Radiative temperature gradient.
    !! @param r radial coordinate of the face (cm).
    !! @param P pressure (erg/cm^3).
    !! @param T temperature (K).
@@ -91,18 +91,18 @@ module turb
    !! @param ierr Tracks errors (output).
    subroutine set_TDC( &
             conv_vel_start, mixing_length_alpha, alpha_TDC_DAMP, alpha_TDC_DAMPR, alpha_TDC_PtdVdt, dt, cgrav, m, report, &
-            mixing_type, scale, chiT, chiRho, L, r, P, T, rho, dV, Cp, opacity, &
+            mixing_type, scale, chiT, chiRho, gradr, r, P, T, rho, dV, Cp, opacity, &
             scale_height, gradL, grada, conv_vel, D, Y_face, gradT, tdc_num_iters, ierr)
       use tdc
       use tdc_support
       real(dp), intent(in) :: conv_vel_start, mixing_length_alpha, alpha_TDC_DAMP, alpha_TDC_DAMPR, alpha_TDC_PtdVdt, dt, cgrav, m, scale
       type(auto_diff_real_star_order1), intent(in) :: &
-         chiT, chiRho, L, r, P, T, rho, dV, Cp, opacity, scale_height, gradL, grada
+         chiT, chiRho, gradr, r, P, T, rho, dV, Cp, opacity, scale_height, gradL, grada
       logical, intent(in) :: report
       type(auto_diff_real_star_order1),intent(out) :: conv_vel, Y_face, gradT, D
       integer, intent(out) :: tdc_num_iters, mixing_type, ierr
       type(tdc_info) :: info
-      type(auto_diff_real_star_order1) :: gradr, grav, Lambda, Gamma
+      type(auto_diff_real_star_order1) :: L, grav, Lambda, Gamma
       real(dp), parameter :: alpha_c = (1d0/2d0)*sqrt_2_div_3
       real(dp), parameter :: lower_bound_Z = -1d2
       real(dp), parameter :: upper_bound_Z = 1d2
@@ -111,8 +111,8 @@ module turb
       include 'formats'
 
       ! Do a call to MLT
-      grav = P / (rho * scale_height)
-      gradr = 3d0 * P * opacity * L / (64 * pi * boltz_sigma * pow4(T) * grav * pow2(r))
+      grav = cgrav * m / pow2(r)
+      L = 64 * pi * boltz_sigma * pow4(T) * grav * pow2(r) * gradr / (3d0 * P * opacity)
       Lambda = mixing_length_alpha * scale_height
       call set_MLT('Cox', mixing_length_alpha, 0d0, 0d0, &
                      chiT, chiRho, Cp, grav, Lambda, rho, P, T, opacity, &
