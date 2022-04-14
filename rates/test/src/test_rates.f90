82a9ff4f921470f852834c340634420daf7cf9f8
Rob Farmer <robert.j.farmer37@gmail.com>
2022-04-14 09:29:11 -0700

    Add back the rates/test for FL 3a

--- rates/test/src/test_rates.f90
+++ rates/test/src/test_rates.f90
@@ -222,7 +222,25 @@
       end subroutine run1
       
    end subroutine test1 
-      
+   
+   subroutine do_test_FL_epsnuc_3alf
+      real(dp) :: T ! temperature
+      real(dp) :: Rho ! density
+      real(dp) :: Y ! helium mass fraction
+      real(dp) :: UE ! electron molecular weight
+      real(dp) :: eps_nuc ! eps_nuc in ergs/g/sec
+      real(dp) :: deps_nuc_dT ! partial wrt temperature
+      real(dp) :: deps_nuc_dRho ! partial wrt density
+      include 'formats'
+      T = 1d7
+      Rho = 1d10
+      Y = 1
+      UE = 2
+      call eval_FL_epsnuc_3alf(T, Rho, Y, UE, eps_nuc, deps_nuc_dT, deps_nuc_dRho)
+      write(*,1) 'FL_epsnuc_3alf', eps_nuc
+      write(*,'(A)')
+   end subroutine do_test_FL_epsnuc_3alf
+
    subroutine do_test_rate_table
       integer :: ierr
       type (T_Factors), target :: tf_rec
@@ -265,6 +283,36 @@
    
    end subroutine do_test_rate_table
    
+   subroutine do_test2_FL_epsnuc_3alf
+      real(dp) :: T ! temperature
+      real(dp) :: Rho ! density
+      real(dp) :: Y ! helium mass fraction
+      real(dp) :: UE ! electron molecular weight
+      real(dp) :: eps_nuc1, eps_nuc2 ! eps_nuc in ergs/g/sec
+      real(dp) :: deps_nuc_dT ! partial wrt temperature
+      real(dp) :: deps_nuc_dRho ! partial wrt density
+      real(dp) :: dT, dRho
+      include 'formats'
+      T = 7.9432823472428218d+07
+      dT = T*1d-8
+      Rho = 3.1622776601683793d+09
+      dRho = Rho*1d-8
+      Y = 1
+      UE = 2
+      call eval_FL_epsnuc_3alf(T, Rho+dRho, Y, UE, eps_nuc1, deps_nuc_dT, deps_nuc_dRho)
+      write(*,1) 'FL_epsnuc_3alf 1', eps_nuc1
+      write(*,'(A)')
+      call eval_FL_epsnuc_3alf(T, Rho, Y, UE, eps_nuc2, deps_nuc_dT, deps_nuc_dRho)
+      write(*,1) 'FL_epsnuc_3alf 2', eps_nuc2
+      write(*,'(A)')
+      write(*,1) 'analytic deps_nuc_dRho', deps_nuc_dRho
+      write(*,1) 'numerical deps_nuc_dRho', (eps_nuc1 - eps_nuc2)/dRho
+      write(*,'(A)')
+      write(*,1) 'analytic dlneps_nuc_dlnRho', deps_nuc_dRho*Rho/eps_nuc2
+      write(*,1) 'numerical dlneps_nuc_dlnRho', (eps_nuc1 - eps_nuc2)/dRho*Rho/eps_nuc2
+      write(*,'(A)')
+   end subroutine do_test2_FL_epsnuc_3alf
+
    subroutine teardown
       call rates_shutdown
    end subroutine teardown
@@ -295,6 +343,7 @@ program test_rates
    call do_test_ecapture
          
    call do_test_rates()
+   call do_test_FL_epsnuc_3alf()
    call do_test_rate_table
 
    call teardown
