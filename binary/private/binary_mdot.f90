92e64c148d386a28a49088a6fb61e9ea36ca7b27
Matthias Fabry <matthias.fabry@kuleuven.be>
2022-06-07 14:26:41 +0200

    Fix to check_implicit_rlo

--- binary/private/binary_mdot.f90
+++ binary/private/binary_mdot.f90
@@ -47,7 +47,7 @@
          type (star_info), pointer :: s
          real(dp) :: function_to_solve, explicit_mdot, q, slope_contact
          integer :: ierr
-         logical :: use_sum
+         logical :: use_sum, detachment
          character (len=90) :: rlo_result
          
          include 'formats'
@@ -59,6 +59,7 @@
          end if
          s => b% s_donor
          use_sum = .false.
+         detachment = .false.
          
          ! NOTE: keep in mind that for mass loss, mdot is negative.
          ! b% mtransfer_rate will be considered valid if function_to_solve = 0
@@ -91,7 +92,7 @@
          end if
 
          if (b% use_other_implicit_function_to_solve) then
-            call b% other_implicit_function_to_solve(b% binary_id, function_to_solve, use_sum, ierr)
+            call b% other_implicit_function_to_solve(b% binary_id, function_to_solve, use_sum, detachment, ierr)
             if (ierr /= 0) then
                write(*,*) 'failed in other_implicit_function_to_solve'
                check_implicit_rlo = retry
@@ -106,7 +107,7 @@
                   rlo_result = 'OK (detached)'
                   call report_rlo_iter
                end if
-               return
+               detachment = .true.
             end if
          else if (b% mdot_scheme == "contact" .and. .not. b% use_other_rlo_mdot .and. .not. b% CE_flag) then
             if (b% point_mass_i /= 0) then
@@ -133,7 +134,7 @@
                      rlo_result = 'OK (detached)'
                      call report_rlo_iter
                   end if
-                  return
+                  detachment = .true.
                end if
             else
                if (q < 1d0) then
@@ -188,7 +189,9 @@
                return
             end if
          end if
-         
+
+         if (detached) return
+
          if (abs(function_to_solve) <= b% implicit_scheme_tolerance) then
             if (b% report_rlo_solver_progress) then
                rlo_result = 'OK (in tol)'
