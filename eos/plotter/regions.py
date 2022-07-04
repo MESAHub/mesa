e8b39fec1e6e6784f2dd464e2f6e004564f2ae87
evbauer <18405113+evbauer@users.noreply.github.com>
2022-07-04 10:49:09 -0400

    [ci skip] update eos region plotting script

--- eos/plotter/regions.py
+++ eos/plotter/regions.py
@@ -6,6 +6,8 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 
+plt.style.use('mesa_eos_regions.mplstyle')
+
 def parse(fname):
     nY, nX = np.loadtxt(fname, max_rows=1, skiprows=3, unpack=True, dtype=int)
     data = np.loadtxt(fname, skiprows=4)
@@ -22,14 +24,15 @@ with open('eos_plotter.dat') as f:
     ylabel = f.readline().strip()
 
 # overwrite with fancier labels
-xlabel = r'$\log_{10}(\rho/{\rm g\,cm^{-3}})$'
-ylabel = r'$\log_{10}(T/{\rm K})$'
+xlabel = r'$\log(\rho/{\rm g\,cm^{-3}})$'
+ylabel = r'$\log(T/{\rm K})$'
 title = r'MESA EOS Regions ($X=0.7$, $Z=0.02$)'
 
 eosDT, Yran, Xran = parse('eos_plotter.dat')
 
+apjcolwidth=3.38
 # set up plot and labels
-fig, ax = plt.subplots(figsize=(5,4))
+fig, ax = plt.subplots(figsize=(apjcolwidth,apjcolwidth*4./5.))
 ax.set_title(title)
 ax.set_xlabel(xlabel)
 ax.set_ylabel(ylabel)
@@ -38,16 +41,19 @@ ax.set_ylim(Yran.min(), Yran.max())
 
 # set up color map
 cmap = mpl.cm.get_cmap("Accent")
-bounds = [-0.5, 0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5, 7.5]
+bounds = [-0.5, 0.5, 1.5, 2.5, 3.5, 5.5, 7.5]
 norm = mpl.colors.BoundaryNorm(bounds, cmap.N)
 
-pcol = ax.pcolormesh(Xran, Yran, eosDT[...,2], shading='nearest', cmap=cmap, norm=norm)
+pcol = ax.pcolormesh(Xran, Yran, eosDT[...,2], shading='nearest', cmap=cmap, norm=norm, rasterized=True)
 pcol.set_edgecolor('face')
-cax = fig.colorbar(pcol, ticks=[0, 1, 2, 3, 4, 5, 6, 7])
+cax = fig.colorbar(pcol, ticks=[0, 1, 2, 3, 4.5, 6.5])
 cax.set_label('')
-cax.ax.set_yticklabels(['blend', 'HELM', 'OPAL/SCVH', 'FreeEOS', 'PC', 'Skye', 'CMS', 'ideal'])
+cax.ax.tick_params(labelsize='x-small')
+cax.ax.minorticks_off()
+cax.ax.set_yticklabels(['blend', 'HELM', 'OPAL/SCVH', 'FreeEOS', 'Skye', 'ideal'])
 
 # save figure
-fig.savefig('eos_regions.png', dpi=300)
+#fig.savefig('eos_regions.png', dpi=300)
+fig.savefig('eos_regions.pdf')
 
 # plt.show()
