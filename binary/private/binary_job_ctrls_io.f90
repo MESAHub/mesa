19d4cd55e27750a74618a60208e5815716658ddb
Matthias Fabry <matthias.fabry@kuleuven.be>
2022-12-20 16:42:38 +0100

    changed binary extra_{}_inlist to array form

--- binary/private/binary_job_ctrls_io.f90
+++ binary/private/binary_job_ctrls_io.f90
@@ -38,11 +38,7 @@
          warn_binary_extra, &
          inlist_names, &
       ! extra files (Maybe overkill with so few inlist parameters)
-         read_extra_binary_job_inlist1, extra_binary_job_inlist1_name, &
-         read_extra_binary_job_inlist2, extra_binary_job_inlist2_name, &
-         read_extra_binary_job_inlist3, extra_binary_job_inlist3_name, &
-         read_extra_binary_job_inlist4, extra_binary_job_inlist4_name, &
-         read_extra_binary_job_inlist5, extra_binary_job_inlist5_name, &
+         read_extra_binary_job_inlist, extra_binary_job_inlist_name, &
          evolve_both_stars, &
          relax_primary_to_th_eq, &
          log_Lnuc_div_L_for_relax_primary_to_th_eq, &
@@ -97,9 +93,10 @@
          type (binary_info), pointer :: b
          integer, intent(in) :: level
          integer, intent(out) :: ierr
-         logical :: read_extra1, read_extra2, read_extra3, read_extra4, read_extra5
-         character (len=strlen) :: message, extra1, extra2, extra3, extra4, extra5
-         integer :: unit
+         logical, dimension(max_extra_inlists) :: read_extra
+         character (len=strlen) :: message
+         character (len=strlen), dimension(max_extra_inlists) :: extra
+         integer :: unit, i
 
          ierr = 0
 
@@ -137,57 +134,18 @@
          call store_binary_job_controls(b, ierr)
 
          ! recursive calls to read other inlists
-
-         read_extra1 = read_extra_binary_job_inlist1
-         read_extra_binary_job_inlist1 = .false.
-         extra1 = extra_binary_job_inlist1_name
-         extra_binary_job_inlist1_name = 'undefined'
-
-         read_extra2 = read_extra_binary_job_inlist2
-         read_extra_binary_job_inlist2 = .false.
-         extra2 = extra_binary_job_inlist2_name
-         extra_binary_job_inlist2_name = 'undefined'
-
-         read_extra3 = read_extra_binary_job_inlist3
-         read_extra_binary_job_inlist3 = .false.
-         extra3 = extra_binary_job_inlist3_name
-         extra_binary_job_inlist3_name = 'undefined'
-
-         read_extra4 = read_extra_binary_job_inlist4
-         read_extra_binary_job_inlist4 = .false.
-         extra4 = extra_binary_job_inlist4_name
-         extra_binary_job_inlist4_name = 'undefined'
-
-         read_extra5 = read_extra_binary_job_inlist5
-         read_extra_binary_job_inlist5 = .false.
-         extra5 = extra_binary_job_inlist5_name
-         extra_binary_job_inlist5_name = 'undefined'
-
-         if (read_extra1) then
-            call read_binary_job_file(b, extra1, level+1, ierr)
-            if (ierr /= 0) return
-         end if
-
-         if (read_extra2) then
-            call read_binary_job_file(b, extra2, level+1, ierr)
-            if (ierr /= 0) return
-         end if
-
-         if (read_extra3) then
-            call read_binary_job_file(b, extra3, level+1, ierr)
-            if (ierr /= 0) return
-         end if
-
-         if (read_extra4) then
-            call read_binary_job_file(b, extra4, level+1, ierr)
-            if (ierr /= 0) return
-         end if
-
-         if (read_extra5) then
-            call read_binary_job_file(b, extra5, level+1, ierr)
-            if (ierr /= 0) return
-         end if
-
+         do i=1, max_extra_inlists
+            read_extra(i) = read_extra_binary_job_inlist(i)
+            read_extra_binary_job_inlist(i) = .false.
+            extra(i) = extra_binary_job_inlist_name(i)
+            extra_binary_job_inlist_name(i) = 'undefined'
+            
+            if (read_extra(i)) then
+               call read_binary_job_file(b, extra(i), level+1, ierr)
+               if (ierr /= 0) return
+            end if
+         end do
+         
       end subroutine read_binary_job_file
 
 
