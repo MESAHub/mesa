1806cdfdd1d9263a4f5ecf1716eb384781f58a69
evbauer <18405113+evbauer@users.noreply.github.com>
2023-05-26 10:44:53 -0400

    Release: r23.05.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -20,8 +20,8 @@ sys.path.insert(0, os.path.abspath('./_ext/'))
 project = 'MESA'
 copyright = '2022, The MESA Team'
 author = 'The MESA Team'
-release = 'main'
-version = 'main'
+release = 'r23.05.1'
+version = 'r23.05.1'
 
 
 # -- General configuration ---------------------------------------------------
