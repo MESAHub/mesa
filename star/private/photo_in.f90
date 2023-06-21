a96eaba4379ecd4df739ee4cdfd5b31b7bb34fc2
Rob Farmer <robert.j.farmer37@gmail.com>
2023-06-21 13:19:35 +0200

    Fix fpe in time smooth for ST
    Time smoothing needs mstar_old but after a restart we have s% generations=0
    so no _old values.

--- star/private/photo_in.f90
+++ star/private/photo_in.f90
@@ -148,7 +148,7 @@
             s% dq(1:nz), s% xa(:,1:nz), s% xh(:,1:nz), &
             s% omega(1:nz), s% j_rot(1:nz), s% mlt_vc(1:nz), s% conv_vel(1:nz), &
             s% D_ST_start(1:nz), s% nu_ST_start(1:nz), & ! needed for ST time smoothing
-            s% have_ST_start_info
+            s% have_ST_start_info, s% mstar_old ! needed for ST time smoothing
 
          call read_part_number(iounit)
          if (failed('rsp_num_periods')) return
