eb12ee04ee4048a1bfae3fa4c4441c1ee2eeac3c
Evan Bauer <18405113+evbauer@users.noreply.github.com>
2023-11-17 16:12:02 -0500

    compile parasite_model.f90

--- turb/private/parasite_model.f90
+++ turb/private/parasite_model.f90
@@ -1,5 +1,7 @@
 module parasite_model
 
+    use const_def, only: dp, pi
+    use num_lib
     use fingering_modes, only: gaml2max
     use kh_instability, only: khparams_from_fingering, gammax_kscan, gammax_minus_lambda
     
@@ -13,8 +15,6 @@ module parasite_model
     public :: nuc
     public :: nut
     public :: gamma_tot
-    public :: parasite_results
-    public :: results_vs_r0
   
     real(dp), parameter :: kb = 1.24_dp
     real(dp), parameter :: CH = 1.66_dp
@@ -40,39 +40,100 @@ module parasite_model
       logical, intent(in) ::  badks_exception, get_kmax
       real(dp), intent(in), optional :: lamhat, l2hat
       real(dp) :: w
-      real(dp), optional :: kmax
+      ! real(dp), optional :: kmax
   
+      integer, parameter  :: IMAX = 50
+      real(dp), parameter :: EPSX = 2e-12_dp
+      real(dp), parameter :: EPSY = 0._dp
+
       real(dp) :: lamhat_, l2hat_
-      real(dp) :: lhat, w1, w2, wb(2)
-      real(dp) :: args(10)
+      real(dp) :: lhat, w1, w2, y1, y2, hb_star, re, rm, dfdx, gammax
+      real(dp), pointer :: rpar(:)
+      integer, pointer  :: ipar(:)
+      integer :: lrpar, lipar
       integer :: ierr
   
       if (present(lamhat)) then
          lamhat_ = lamhat
          l2hat_ = l2hat
       else
-         call gaml2max_new(pr, tau, r0, lamhat_, l2hat_) 
+         call gaml2max(pr, tau, r0, lamhat_, l2hat_, ierr)
       end if
   
       lhat = sqrt(l2hat_)
   
       ! Set bracketing interval
       w1 = 2.0_dp*pi*lamhat_/lhat
-      w2 = sqrt(2.0_dp*hb)
-      wb = [w1, w1 + w2]
-  
+      w2 = w1 + sqrt(2.0_dp*hb)
+      
+      lrpar = 7 + size(ks)
+      lipar = 1
+      allocate(rpar(lrpar))
+      allocate(ipar(lipar))
+      
       ! Arguments to pass
-      args = [lamhat_, lhat, hb, pr, db, delta, ks, n, ideal, badks_exception, CH]
+      rpar = [lamhat_, lhat, hb, pr, db, delta, CH, ks]
+      ipar = [n]
+      dfdx = 0d0 ! unused for bracket search
+      
+      y1 = gx_m_lam(w1, dfdx, lrpar, rpar, lipar, ipar, ierr)
+      y2 = gx_m_lam(w2, dfdx, lrpar, rpar, lipar, ipar, ierr)
   
       ! Call bracketed root finder
-      call brent(gammax_minus_lambda, w, wb, args, ierr)
-  
-      if (present(get_kmax)) then
-         call khparams_from_fingering(w, lhat, hb, pr, db, m2, re, rm)
-         call gammax_kscan(delta, m2, re, rm, ks, n, ideal, badks_exception, kmax) 
+      w = safe_root_with_brackets( &
+           gx_m_lam, w1, w2, y1, y2, imax, epsx, epsy, lrpar, rpar, lipar, ipar, ierr)
+
+      ! needs more work on gammax_kscan
+      if (get_kmax) then
+         call khparams_from_fingering(w, lhat, hb, pr, db, hb_star, re, rm)
+         gammax = gammax_kscan(delta, hb_star, re, rm, ks, n, .false., badks_exception) !, kmax) 
       end if
+
+      deallocate(rpar)
+      deallocate(ipar)
   
     end function wf
+
+    ! wrapper for gammax_minus_lambda for use with safe_root_with_brackets
+    real(dp) function gx_m_lam(x, dfdx, lrpar, rpar, lipar, ipar, ierr) result(f)
+      
+      real(dp), intent(in)             :: x
+      real(dp), intent(out)            :: dfdx
+      integer, intent(in)              :: lrpar
+      real(dp), intent(inout), pointer :: rpar(:)
+      integer, intent(in)              :: lipar
+      integer, intent(inout), pointer  :: ipar(:)
+      integer, intent(out)             :: ierr
+
+      dfdx = 0d0 ! unused for bracket search
+      
+      if (lrpar /= 8 .OR. lipar /= 2) then
+         ierr = -1
+         return
+      end if
+      
+      associate( &
+           w => x, &
+           lamhat => rpar(1), &
+           lhat => rpar(2), &
+           hb => rpar(3), &
+           pr => rpar(4), &
+           db => rpar(5), &
+           delta => rpar(6), &
+           CH => rpar(7), &
+           ks => rpar(8:), & 
+           n => ipar(1))
+
+        f = gammax_minus_lambda(w, lamhat, lhat, hb, pr, db, delta, ks, n, .false., .false.)
+        ! ideal = .false., and ignoring badks_exception right now
+        
+      end associate
+
+      ierr = 0
+
+      return
+      
+    end function gx_m_lam
   
     function hg19_eq32(w, pr, tau, r0, hb, CH) result(f)
   
@@ -82,8 +143,9 @@ module parasite_model
       real(dp) :: f
   
       real(dp) :: lamhat, lhat, l2hat
+      integer :: ierr
   
-      call gaml2max(pr, tau, r0, lamhat, l2hat)
+      call gaml2max(pr, tau, r0, lamhat, l2hat, ierr)
       lhat = sqrt(l2hat)
   
       f = 0.5_dp*w**2 - hb - (CH*lamhat/(0.42_dp*lhat))**1.5 * sqrt(w)
@@ -98,8 +160,9 @@ module parasite_model
       real(dp) :: dfdw
   
       real(dp) :: lamhat, lhat, l2hat
+      integer :: ierr
   
-      call gaml2max(pr, tau, r0, lamhat, l2hat)
+      call gaml2max(pr, tau, r0, lamhat, l2hat, ierr)
       lhat = sqrt(l2hat)
   
       dfdw = w - 0.5_dp*(CH*lamhat/(0.42_dp*lhat))**1.5/sqrt(w)
@@ -113,11 +176,12 @@ module parasite_model
       real(dp), intent(in) :: pr, tau, r0, hb, CH
       real(dp) :: w
   
+      real(dp) :: lamhat, lhat, l2hat
       real(dp) :: w0
       integer :: ierr
   
       ! Initial guess
-      call gaml2max(pr, tau, r0, lamhat, l2hat)
+      call gaml2max(pr, tau, r0, lamhat, l2hat, ierr)
       lhat = sqrt(l2hat)
       w0 = max(sqrt(2.0_dp*hb), 2.0_dp*pi*lamhat/lhat)
   
@@ -163,87 +227,5 @@ module parasite_model
       gamma = r0*nut_/(tau*nuc_)
   
     end function gamma_tot
-  
-    function parasite_results(r0, hb, pr, tau, db, ks, n, lamhat, l2hat, eq32, double_n, &
-         delta, ideal, CH, badks_exception) result(results)
-  
-      ! Get full set of results for parasite model
-  
-      real(dp), intent(in) :: r0, hb, pr, tau, db
-      real(dp), intent(in) :: ks(:)
-      integer, intent(in) :: n
-      real(dp), intent(in) :: lamhat, l2hat 
-      logical, intent(in) :: eq32, double_n, ideal, badks_exception
-      real(dp), intent(in) :: delta, CH  
-      type(dict) :: results
-  
-      real(dp) :: wf, fc, ft, nu_c, nu_t, gamma_tot
-      real(dp) :: m2, re, kmax
-  
-      if (eq32) then
-         wf = wf_hg19(pr, tau, r0, hb, CH)
-      else if (double_n) then
-         call wf(pr, tau, r0, hb, db, ks, 2*n-1, delta, ideal, badks_exception, .true., &
-              lamhat, l2hat, wf, kmax)
-      else
-         call wf(pr, tau, r0, hb, db, ks, n, delta, ideal, badks_exception, .true., & 
-              lamhat, l2hat, wf, kmax)
-      end if
-  
-      fc = kb*wf**2/(r0*(lamhat + tau*l2hat))
-      ft = kb*wf**2/(lamhat + l2hat)
-      nu_c = nuc(tau, wf, lamhat, l2hat)
-      nu_t = nut(wf, lamhat, l2hat)
-      gamma_tot = gamma_tot(tau, r0, wf, lamhat, l2hat, kb)
-  
-      call khparams_from_fingering(wf, sqrt(l2hat), hb, pr, db, m2, re, rm)
-  
-      results = dict(names=["FC", "FT", "NuC", "NuT", "gammatot", "wf", "Re-star", "HB-star"])
-  
-      if (.not. eq32) then
-         results%names(size(results%names)) = "kmax-star"
-         results%vals(size(results%vals)) = kmax  
-      end if
-  
-      results%vals = [fc, ft, nu_c, nu_t, gamma_tot, wf, re, m2]
-  
-    end function parasite_results
-  
-    function results_vs_r0(r0s, hb, pr, tau, db, ks, n, lamhats, l2hats, eq32, double_n, &
-         delta, ideal, CH, badks_exception) result(results)
-  
-      ! Scan over density ratio
-  
-      real(dp), intent(in) :: r0s(:), hb, pr, tau, db  
-      real(dp), intent(in) :: ks(:), lamhats(:), l2hats(:)
-      integer, intent(in) :: n
-      logical, intent(in) :: eq32, double_n, ideal, badks_exception
-      real(dp), intent(in) :: delta, CH
-      type(dict) :: results
-  
-      integer :: i
-      type(dict) :: result_ri
-  
-      results = dict(names=["FC", "FT", "NuC", "NuT", "gammatot", "wf", "Re-star", "HB-star"])
-  
-      if (.not. eq32) then
-         results%names(size(results%names)) = "kmax-star"
-      end if
-  
-      do i = 1, size(r0s)
-         if (eq32) then
-            result_ri = parasite_results(r0s(i), hb, pr, tau, db, ks, n, &
-                 lamhats(i), l2hats(i), eq32=eq32, CH=CH)
-         else
-            result_ri = parasite_results(r0s(i), hb, pr, tau, db, ks, n, &
-                 lamhats(i), l2hats(i), eq32=eq32, double_n=double_n, &
-                 delta=delta, ideal=ideal, CH=CH, badks_exception=badks_exception)
-         end if
-  
-         results%vals(1:size(result_ri%vals)) = result_ri%vals
-  
-      end do
-  
-    end function results_vs_r0
-  
+    
   end module parasite_model
