0aa927d28084d9d5d74ed0f6868398bf0c50f21c
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-07-28 21:32:34 -0400

    build: add new build system
    This commit introduces a new build system (still based on make files).
    Reasons for replacing the previous build system were:
    
    - the previous build system had a lot of duplication between the
      modules, both in terms of make files and shell scripts.
    - parallelisation was limited. The new build system parallelises better
      within a module, and between modules.
    - build scripts and output from the build ended up in the same folder.
      With the new system, all build output files are stored in the folder
      ``build`` in the repository root, making cleaning build files
      equivalent to deleting this folder
    
    For the details of the implementation, see the newly added documentation
    (Developing -> Build system).
    
    Some further notable changes:
    
    - makedepf90 has been replaced by a perl script that scans the fortran
      source files. This allows for better flexibility in construction the
      make files.
    - Currently, `pymesa` does not work with the new build system, as the
      new system does not provide a way to build shared libraries for every
      module.
    - Dependencies are being discovered using pkg-config. This change
      makes it easier to build MESA without the SDK, as linux distributions
      typically ship with pkg-config files.

---
+++ make/defaults-module.mk
@@ -0,0 +1,15 @@
+# A bit of sed fun to ensure MAKE_DIR does not end with a '/'
+# This is not a problem, but it makes the output nicer
+MAKE_DIR := $(shell echo $(dir $(lastword $(MAKEFILE_LIST))) | sed "s|/$$||")
+
+ifneq ($(.SHELLSTATUS),0)
+  $(error extracting `make` directory based on defaults-module.mk location failed)
+endif
+
+BUILD_DIR ?= build
+COMPILER ?= gfortran
+VERSION ?= $(file <$(MAKE_DIR)../data/version_number)
+PROFILE ?= release-with-dbg-info
+WITH_OPENMP ?= yes
+WITH_CRLIBM ?= yes
+PREFIX ?= $(MAKE_DIR)/..
