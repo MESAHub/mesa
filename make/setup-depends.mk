3f31cc771c4dab6e91494057e9d5e3c4d55643f5
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2024-07-28 22:21:43 -0400

    build: support current SDK version
    This commit adds the necessary pkg-config files until the SDK ships
    them.

--- make/setup-depends.mk
+++ make/setup-depends.mk
@@ -6,12 +6,25 @@ endif
 
 include $(MAKE_DIR)/subdirs.mk
 
+PKG_CONFIG_FLAGS =
+
+ifneq ($(MESASDK_ROOT),)
+  PKG_CONFIG_PATH := $(MAKE_DIR)/pkgconfig:$(PKG_CONFIG_PATH)
+  PKG_CONFIG_FLAGS += --define-variable=MESASDK_ROOT=$(MESASDK_ROOT)
+
+  ifeq ($(WITH_CRLIBM),yes)
+    PKG_CONFIG_FLAGS += --define-variable=MATH_SLOT=crmath
+  else
+    PKG_CONFIG_FLAGS += --define-variable=MATH_SLOT=default
+  endif
+endif
+
 PKG_CONFIG_PATH := $(shell BUILD_DIR=$(BUILD_DIR_) $(MAKE_DIR)/gen-pkgconfig-path $(SUBDIRS)):$(PKG_CONFIG_PATH)
 export PKG_CONFIG_PATH
 
 define pkg-config-inner
 ifneq ($(2),)
-  TMP_VAR := $$(shell pkg-config $(1) $(2))
+  TMP_VAR := $$(shell PKG_CONFIG_PATH=$(PKG_CONFIG_PATH) pkg-config $(PKG_CONFIG_FLAGS) $(1) $(2))
   ifneq ($$(.SHELLSTATUS),0)
     $$(warning PKG_CONFIG_PATH is $$(PKG_CONFIG_PATH))
     $$(error pkg-config failed to find some of $(2), check PKG_CONFIG_PATH is correct)
