8f72ddb5bdb3dd7354960b35c52006b643a8ad15
pmocz <philip.mocz@gmail.com>
2024-08-21 20:54:45 -0400

    more unused

--- star/private/ctrls_io.f90
+++ star/private/ctrls_io.f90
@@ -639,7 +639,6 @@
  integer, intent(in) :: level
  integer, intent(out) :: ierr
  logical, dimension(max_extra_inlists) :: read_extra
- character (len=strlen) :: message
  character (len=strlen), dimension(max_extra_inlists) :: extra
  integer :: unit, i
 
