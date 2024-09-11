3c481a6ab5cf0d4e8f8ab4565b73a1b4d51527f5
pmocz <philip.mocz@gmail.com>
2024-09-11 16:09:32 -0400

    add stop_for_bad_nums guard

--- star/private/rsp.f90
+++ star/private/rsp.f90
@@ -681,7 +681,7 @@
             if (s% RSP_report_limit_dt) then
                write(*,4) 'limit to RSP_max_dt_times_min_dr_div_cs', s% model_number, max_dt, s% dt
             end if
-            call mesa_error(__FILE__,__LINE__,'do1_step 1')
+            if (s% stop_for_bad_nums) call mesa_error(__FILE__,__LINE__,'do1_step 1')
             s% dt = max_dt
          end if
          
