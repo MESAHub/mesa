3ac3c3a977d298f4790583de1b65b49e80127652
Warrick Ball <W.H.Ball@bham.ac.uk>
2024-12-30 19:34:45 +0000

    ionization: port to new build system

--- make/subdirs.mk
+++ make/subdirs.mk
@@ -1,1 +1,1 @@
-SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap
+SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap ionization
