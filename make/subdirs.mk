7a3757104c4e3bf462fda62a6d369be6ddf62917
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2025-01-03 16:12:19 +0100

    astero: port to new build system

--- make/subdirs.mk
+++ make/subdirs.mk
@@ -1,6 +1,6 @@
 include $(MAKE_DIR)/defaults-module.mk
 
-SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap ionization atm turb star_data star
+SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap ionization atm turb star_data star astero
 
 ifeq ($(WITH_ADIPLS),yes)
   SUBDIRS += adipls
