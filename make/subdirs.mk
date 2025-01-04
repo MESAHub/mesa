c27d6e2a7e1a2cc0971c0088630e4f861e11e488
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2025-01-04 12:37:30 +0100

    binary: port to new build system

--- make/subdirs.mk
+++ make/subdirs.mk
@@ -1,6 +1,6 @@
 include $(MAKE_DIR)/defaults-module.mk
 
-SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap ionization atm turb star_data star astero
+SUBDIRS := const utils math mtx auto_diff num interp_1d interp_2d chem eos forum colors rates neu net gyre kap ionization atm turb star_data star astero binary
 
 ifeq ($(WITH_ADIPLS),yes)
   SUBDIRS += adipls
