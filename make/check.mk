8cac0e6fefd163c1865945eadb207f9bdf1a9891
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2025-01-23 14:10:45 +0100

    build: reenable and fix all tests

--- make/check.mk
+++ make/check.mk
@@ -2,17 +2,23 @@ CHECKER := $(BUILD_DIR_MODULE)/bin/checker
 CHECK_RESULT_DIR := $(BUILD_DIR_MODULE)
 CHECK_RESULTS := $(CHECK_RESULT_DIR)/check-results
 CHECK_DIFF_PROG ?= diff -b
+CHECK_FILTER_CACHE_WRITE ?= yes
 
 $(CHECKER): $(ALL_DEPS) $(OBJS_CHECK) | $(BUILD_DIR_MODULE)/bin/
 	$(EXECUTABLE) -o $(CHECKER) $(OBJS_CHECK) $(call pkg-config,--libs,mesa-$(MODULE_NAME))
 
+ifeq ($(CHECK_FILTER_CACHE_WRITE),yes)
+CHECK_FILTER_PROG ?= grep -Ev "^(write)|(create rate data for)|( read )|( write )|(                           number not already in cache:)"
+else
+CHECK_FILTER_PROG ?= cat
+endif
+
 ifneq ($(OBJS_CHECK),)
   $(CHECK_RESULTS) : $(CHECKER) | $(CHECK_RESULT_DIR)/
-	pushd test > /dev/null ; ../$(CHECKER) > ../$(CHECK_RESULTS); popd > /dev/null
+	cd test; ../$(CHECKER) | $(CHECK_FILTER_PROG) > ../$(CHECK_RESULTS)
 	$(CHECK_DIFF_PROG) $(CHECK_RESULTS) $(CHECK_RESULTS_GOLDEN)
 
   check: $(CHECK_RESULTS)
 else
   check:
-	$(error No test sources specified)
 endif
