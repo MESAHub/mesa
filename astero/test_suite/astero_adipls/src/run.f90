0cc7f0ff5d9f84934919ce428b0faa6447a53e41
pmocz <philip.mocz@gmail.com>
2025-02-12 08:30:37 -0500

    bugfix implicit none in astero

--- astero/test_suite/astero_adipls/src/run.f90
+++ astero/test_suite/astero_adipls/src/run.f90
@@ -1,6 +1,6 @@
 program run
-   implicit none
    use astero_lib, only: run_star_astero
    use run_star_extras, only: extras_controls
+   implicit none
    call run_star_astero(extras_controls)
 end program run
