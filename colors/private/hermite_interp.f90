ddd6cf7ae7564547ef0006ba8ea40671251856fe
Niall Miller <niall.j.miller@gmail.com>
2025-07-28 13:17:41 -0600

    Cleaned up Hermite interpolation: fewer superfluous checks

--- colors/private/hermite_interp.f90
+++ colors/private/hermite_interp.f90
@@ -193,9 +193,8 @@ contains
       close (unit)
    end subroutine load_binary_data
 
-!---------------------------------------------------------------------------
-   ! Main 3D Hermite interpolation function
-   !---------------------------------------------------------------------------
+
+
    function hermite_tensor_interp3d(x_val, y_val, z_val, x_grid, y_grid, &
                                     z_grid, f_values) result(f_interp)
       real(dp), intent(in) :: x_val, y_val, z_val
@@ -211,6 +210,7 @@ contains
       real(dp) :: sum
       integer :: ix, iy, iz
       real(dp) :: h_x(2), h_y(2), h_z(2)
+      real(dp) :: hx_d(2), hy_d(2), hz_d(2)
 
       ! Find containing cell and parameter values
       call find_containing_cell(x_val, y_val, z_val, x_grid, y_grid, z_grid, &
@@ -247,54 +247,31 @@ contains
          end do
       end do
 
+      ! Precompute Hermite basis functions and derivatives
+      h_x  = [h00(t_x), h01(t_x)]
+      hx_d = [h10(t_x), h11(t_x)]
+      h_y  = [h00(t_y), h01(t_y)]
+      hy_d = [h10(t_y), h11(t_y)]
+      h_z  = [h00(t_z), h01(t_z)]
+      hz_d = [h10(t_z), h11(t_z)]
+
+      ! Final interpolation sum
       sum = 0.0_dp
-
-      ! Function values
-      h_x = [h00(t_x), h01(t_x)]
-      h_y = [h00(t_y), h01(t_y)]
-      h_z = [h00(t_z), h01(t_z)]
       do iz = 1, 2
          do iy = 1, 2
             do ix = 1, 2
-               sum = sum + h_x(ix)*h_y(iy)*h_z(iz)*values(ix, iy, iz)
-            end do
-         end do
-      end do
-
-      ! x-derivatives
-      h_x = [h10(t_x), h11(t_x)]
-      do iz = 1, 2
-         do iy = 1, 2
-            do ix = 1, 2
-               sum = sum + h_x(ix)*h_y(iy)*h_z(iz)*dx*dx_values(ix, iy, iz)
-            end do
-         end do
-      end do
-
-      ! y-derivatives
-      h_x = [h00(t_x), h01(t_x)]
-      h_y = [h10(t_y), h11(t_y)]
-      do iz = 1, 2
-         do iy = 1, 2
-            do ix = 1, 2
-               sum = sum + h_x(ix)*h_y(iy)*h_z(iz)*dy*dy_values(ix, iy, iz)
-            end do
-         end do
-      end do
-
-      ! z-derivatives
-      h_y = [h00(t_y), h01(t_y)]
-      h_z = [h10(t_z), h11(t_z)]
-      do iz = 1, 2
-         do iy = 1, 2
-            do ix = 1, 2
-               sum = sum + h_x(ix)*h_y(iy)*h_z(iz)*dz*dz_values(ix, iy, iz)
+               sum = sum + h_x(ix)*h_y(iy)*h_z(iz)     * values(ix, iy, iz)
+               sum = sum + hx_d(ix)*h_y(iy)*h_z(iz)    * dx * dx_values(ix, iy, iz)
+               sum = sum + h_x(ix)*hy_d(iy)*h_z(iz)    * dy * dy_values(ix, iy, iz)
+               sum = sum + h_x(ix)*h_y(iy)*hz_d(iz)    * dz * dz_values(ix, iy, iz)
             end do
          end do
       end do
 
       f_interp = sum
    end function hermite_tensor_interp3d
+
+
 
    !---------------------------------------------------------------------------
    ! Find the cell containing the interpolation point
