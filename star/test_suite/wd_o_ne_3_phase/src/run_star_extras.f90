039cf6aadbc43da63f86693426d75cf5b96e4ebe
evbauer <bauer39@llnl.gov>
2025-09-30 10:45:37 -0700

    fix script permissions, more fortitude fixes

--- star/test_suite/wd_o_ne_3_phase/src/run_star_extras.f90
+++ star/test_suite/wd_o_ne_3_phase/src/run_star_extras.f90
@@ -9,7 +9,7 @@
 !   by the free software foundation; either version 2 of the license, or
 !   (at your option) any later version.
 !
-!   mesa is distributed in the hope that it will be useful, 
+!   mesa is distributed in the hope that it will be useful,
 !   but without any warranty; without even the implied warranty of
 !   merchantability or fitness for a particular purpose.  see the
 !   gnu library general public license for more details.
@@ -19,23 +19,23 @@
 !   foundation, inc., 59 temple place, suite 330, boston, ma 02111-1307 usa
 !
 ! ***********************************************************************
- 
+
       module run_star_extras
       use star_lib
       use star_def
       use const_def
-      use math_lib   
-      implicit none 
+      use math_lib
+      implicit none
       ! these routines are called by the standard run_star check_model
       contains
-      
+
       subroutine extras_controls(id, ierr)
          integer, intent(in) :: id
          integer, intent(out) :: ierr
          type (star_info), pointer :: s
          ierr = 0
          call star_ptr(id, s, ierr)
-         if (ierr /= 0) return         
+         if (ierr /= 0) return
          ! this is the place to set any procedure pointers you want to change
          ! e.g., other_wind, other_mixing, other_energy  (see star_data.inc)
          ! the extras functions in this file will not be called
@@ -49,13 +49,13 @@
          s% how_many_extra_history_columns => how_many_extra_history_columns
          s% data_for_extra_history_columns => data_for_extra_history_columns
          s% how_many_extra_profile_columns => how_many_extra_profile_columns
-         s% data_for_extra_profile_columns => data_for_extra_profile_columns  
+         s% data_for_extra_profile_columns => data_for_extra_profile_columns
          s% how_many_extra_history_header_items => how_many_extra_history_header_items
          s% data_for_extra_history_header_items => data_for_extra_history_header_items
          s% how_many_extra_profile_header_items => how_many_extra_profile_header_items
          s% data_for_extra_profile_header_items => data_for_extra_profile_header_items
       end subroutine extras_controls
-        
+
       subroutine extras_startup(id, restart, ierr)
          integer, intent(in) :: id
          logical, intent(in) :: restart
@@ -65,7 +65,7 @@
          call star_ptr(id, s, ierr)
          if (ierr /= 0) return
       end subroutine extras_startup
-      
+
       integer function extras_start_step(id)
          integer, intent(in) :: id
          integer :: ierr
@@ -86,7 +86,7 @@
          ierr = 0
          call star_ptr(id, s, ierr)
          if (ierr /= 0) return
-         extras_check_model = keep_going         
+         extras_check_model = keep_going
          if (.false. .and. s% star_mass_h1 < 0.35d0) then
             ! stop when star hydrogen mass drops to specified level
             extras_check_model = terminate
@@ -109,31 +109,31 @@
          iXNe22 = -1
          iXNa = -1
          iXMg = -1
-         iXHe = -1         
+         iXHe = -1
          do k = 1,s% species
-             if (chem_isos% name(s% chem_id(k)) .eq. 'c12') then
+             if (chem_isos% name(s% chem_id(k)) == 'c12') then
                 iXC = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'o16') then
+             if (chem_isos% name(s% chem_id(k)) == 'o16') then
                 iXO = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'ne20') then
+             if (chem_isos% name(s% chem_id(k)) == 'ne20') then
                 iXNe20 = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'ne22') then
+             if (chem_isos% name(s% chem_id(k)) == 'ne22') then
                 iXNe22 = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'na23') then
+             if (chem_isos% name(s% chem_id(k)) == 'na23') then
                 iXNa = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'mg24') then
+             if (chem_isos% name(s% chem_id(k)) == 'mg24') then
                 iXMg = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'he4') then
+             if (chem_isos% name(s% chem_id(k)) == 'he4') then
                 iXHe = k
              end if
          end do
-         if (iXC .eq. -1 .or. iXO .eq. -1 .or. iXNe20 .eq. -1 .or. iXNe22 .eq. -1 .or. iXNa .eq. -1 .or. iXMg .eq. -1 .or. iXHe .eq. -1) then
+         if (iXC == -1 .or. iXO == -1 .or. iXNe20 == -1 .or. iXNe22 == -1 .or. iXNa == -1 .or. iXMg == -1 .or. iXHe == -1) then
             write (*,*) 'Could not find elements specified!!'
          end if
          ! Find the base of the oxygen layer
@@ -163,7 +163,7 @@
          if (ierr /= 0) return
          how_many_extra_history_columns = 3
       end function how_many_extra_history_columns
-      
+
       subroutine data_for_extra_history_columns(id, n, names, vals, ierr)
          use chem_def, only: i3alf
          integer, intent(in) :: id, n
@@ -174,28 +174,28 @@
          integer :: i_max,kc,k
          ierr = 0
          call star_ptr(id, s, ierr)
-         if (ierr /= 0) return  
+         if (ierr /= 0) return
          ! note: do NOT add the extras names to history_columns.list
          ! the history_columns.list is only for the built-in history column options.
          ! it must not include the new column names you are adding here.
          ! find the maximum in the helium burning rate
-         i_max = maxloc(s% eps_nuc_categories(i3alf,:), dim=1)    
+         i_max = maxloc(s% eps_nuc_categories(i3alf,:), dim=1)
          ! to find the maximum in the total epsilon:
-         !i_max = maxloc(s% eps_nuc, dim=1)                  
+         !i_max = maxloc(s% eps_nuc, dim=1)
          names(1) = "max_eps_he_lgT"
          vals(1) = log10(s% T(i_max))
          names(2) = "mass_core_cryst"
          vals(2) = s% crystal_core_boundary_mass
          do k = s%nz,1,-1
-               if(s% m(k) .ge. s% crystal_core_boundary_mass) then
+               if(s% m(k) >= s% crystal_core_boundary_mass) then
                   kc = k
                   exit
                end if
          end do
          names(3) = "r_core_cryst"
-         vals(3) = exp(s% lnR(kc))   
+         vals(3) = exp(s% lnR(kc))
       end subroutine data_for_extra_history_columns
- 
+
       integer function how_many_extra_profile_columns(id)
          integer, intent(in) :: id
          integer :: ierr
@@ -205,7 +205,7 @@
          if (ierr /= 0) return
          how_many_extra_profile_columns = 14
       end function how_many_extra_profile_columns
-      
+
       subroutine data_for_extra_profile_columns(id, n, nz, names, vals, ierr)
          use chem_def
          use eos_def
@@ -222,10 +222,10 @@
          real(dp) :: P1, S1, my_chiT, eps, chiX_C12, chiX_O16, chiX_Ne20, chiX_Ne22, chiX_Na23, chiX_Mg24,chiX_He4, bs_C12, bs_O16, bs_Ne20, bs_Ne22, bs_Na23, bs_Mg24, bs_He4, mu1  !!!
          !real(dp) :: chimu_c12,chimu_o16,chimu_ne20,chimu_mg24
          real(dp) :: plnxc_plnye, plnxo_plnye, plnxne20_plnye, plnxne22_plnye, plnxmg_plnye, ln_ye
-         type (star_info), pointer :: s     
+         type (star_info), pointer :: s
          ierr = 0
          call star_ptr(id, s, ierr)
-         if (ierr /= 0) return     
+         if (ierr /= 0) return
          allocate(d_dxa(num_eos_d_dxa_results,s% species))
          allocate(xa1_c12(s% species))
          allocate(xa1_o16(s% species))
@@ -233,7 +233,7 @@
          allocate(xa1_ne22(s% species))
          allocate(xa1_na23(s% species))
          allocate(xa1_mg24(s% species))
-         allocate(xa1_he4(s% species))              
+         allocate(xa1_he4(s% species))
          names(1) = 'chiX_C12'
          names(2) = 'chiX_O16'
          names(3) = 'chiX_Ne20'
@@ -254,31 +254,31 @@
          iXNe22 = -1
          iXNa = -1
          iXMg = -1
-         iXHe = -1       
+         iXHe = -1
          do k = 1,s% species
-             if (chem_isos% name(s% chem_id(k)) .eq. 'c12') then
+             if (chem_isos% name(s% chem_id(k)) == 'c12') then
                 iXC = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'o16') then
+             if (chem_isos% name(s% chem_id(k)) == 'o16') then
                 iXO = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'ne20') then
+             if (chem_isos% name(s% chem_id(k)) == 'ne20') then
                 iXNe20 = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'ne22') then
+             if (chem_isos% name(s% chem_id(k)) == 'ne22') then
                 iXNe22 = k
              end if
-              if (chem_isos% name(s% chem_id(k)) .eq. 'na23') then
+              if (chem_isos% name(s% chem_id(k)) == 'na23') then
                 iXNa = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'mg24') then
+             if (chem_isos% name(s% chem_id(k)) == 'mg24') then
                 iXMg = k
              end if
-             if (chem_isos% name(s% chem_id(k)) .eq. 'he4') then
+             if (chem_isos% name(s% chem_id(k)) == 'he4') then
                 iXHe = k
              end if
          end do
-         if (iXC .eq. -1 .or. iXO .eq. -1 .or. iXNe20 .eq. -1 .or. iXNe22 .eq. -1 .or. iXMg .eq. -1 .or. iXNa .eq. -1 .or. iXHe .eq. -1) then
+         if (iXC == -1 .or. iXO == -1 .or. iXNe20 == -1 .or. iXNe22 == -1 .or. iXMg == -1 .or. iXNa == -1 .or. iXHe == -1) then
             write (*,*) 'Could not find elements specified!!'
          end if
          do k = 1, nz
@@ -288,7 +288,7 @@
                s% rho(k), log10(s% rho(k)), s% T(k), log10(s% T(k)), &
                res, d_dlnd, d_dlnT, d_dxa, ierr)
             P1 = res(i_lnPgas)
-            S1 = exp(res(i_lnS)) 
+            S1 = exp(res(i_lnS))
             mu1= res(i_mu)!s% mu(k)
             ln_ye=res(i_lnfree_e)
             xa1_c12 = s% xa(:, k)
@@ -366,7 +366,7 @@
             vals(k,12) = bs_Na23
             vals(k,13) = bs_Mg24
             vals(k,14) = bs_He4
-          end do   
+          end do
       end subroutine data_for_extra_profile_columns
 
       integer function how_many_extra_history_header_items(id)
@@ -429,7 +429,7 @@
          call star_ptr(id, s, ierr)
          if (ierr /= 0) return
          extras_finish_step = keep_going
-         ! to save a profile, 
+         ! to save a profile,
             ! s% need_to_save_profiles_now = .true.
          ! to update the star log,
             ! s% need_to_update_history_now = .true.
@@ -437,7 +437,7 @@
          ! by default, indicate where (in the code) MESA terminated
          if (extras_finish_step == terminate) s% termination_code = t_extras_finish_step
       end function extras_finish_step
-           
+
       subroutine extras_after_evolve(id, ierr)
          integer, intent(in) :: id
          integer, intent(out) :: ierr
@@ -447,4 +447,4 @@
          if (ierr /= 0) return
       end subroutine extras_after_evolve
       end module run_star_extras
-      
+
