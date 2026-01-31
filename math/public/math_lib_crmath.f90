1a6a387ded715829476fffdd192725d50ad94af4
Vincent Vanlaer <vincent.vanlaer@kuleuven.be>
2026-01-31 18:02:05 +0100

    math: remove ieee_arithmetic import
    Importing any of the ieee modules has a somewhat unexpected effect. Each
    procedure that has an ieee module in scope will (1) save the FPU state
    at the start and (2) restore it at the end, adding any flags that were
    raised by the procedure. Since the `use ieee_arithmetic` is included in
    the main math module directly, everything that imports math will be
    effectively be importing an ieee module. This has a large impact on
    performance (for example, the `low_z` test case runs in 2/3 the time
    with this patch).

--- math/public/math_lib_crmath.f90
+++ math/public/math_lib_crmath.f90
@@ -27,7 +27,6 @@ module math_lib
   use math_def
 
   use crmath
-  use IEEE_ARITHMETIC
 
   implicit none
 
@@ -130,7 +129,7 @@ contains
     real(dp), intent(in) :: x
     real(dp)             :: log_x
 
-    if (.NOT. IEEE_IS_FINITE(x)) then
+    if (is_nan(x) .or. is_inf(x)) then
 
        log_x = -99._dp
 
@@ -148,7 +147,7 @@ contains
     real(dp), intent(in) :: x
     real(dp)             :: log10_x
 
-    if (.NOT. IEEE_IS_FINITE(x)) then
+    if (is_nan(x) .or. is_inf(x)) then
 
        log10_x = -99._dp
 
