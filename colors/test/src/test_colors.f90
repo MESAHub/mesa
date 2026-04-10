05aa323c620af4eb029bdd6661a22791e3c1433b
Niall Miller <niall.j.miller@gmail.com>
2026-04-10 11:26:51 -0600

    double precision fix.

--- colors/test/src/test_colors.f90
+++ colors/test/src/test_colors.f90
@@ -172,7 +172,7 @@ program test_colors
       end if
       write(*,'(a, a)') '# case: ', trim(adjustl(labels(j)))
       do k = 1, n_cols
-         write(*,'(a40, 1pe22.12)') trim(col_names(k)), col_vals(k)
+         write(*,'(a40, 1pe26.16)') trim(col_names(k)), col_vals(k)
       end do
    end do
 
@@ -192,7 +192,7 @@ program test_colors
       end if
       write(*,'(a, f6.2)') '# MH= ', sweep_meta(j)
       do k = 1, n_cols
-         write(*,'(a40, 1pe22.12)') trim(col_names(k)), col_vals(k)
+         write(*,'(a40, 1pe26.16)') trim(col_names(k)), col_vals(k)
       end do
    end do
 
@@ -212,7 +212,7 @@ program test_colors
       end if
       write(*,'(a, f6.2)') '# logg= ', sweep_logg(j)
       do k = 1, n_cols
-         write(*,'(a40, 1pe22.12)') trim(col_names(k)), col_vals(k)
+         write(*,'(a40, 1pe26.16)') trim(col_names(k)), col_vals(k)
       end do
    end do
 
@@ -232,7 +232,7 @@ program test_colors
       end if
       write(*,'(a, f10.1)') '# Teff= ', sweep_teff(j)
       do k = 1, n_cols
-         write(*,'(a40, 1pe22.12)') trim(col_names(k)), col_vals(k)
+         write(*,'(a40, 1pe26.16)') trim(col_names(k)), col_vals(k)
       end do
    end do
 
@@ -250,7 +250,7 @@ program test_colors
 
    write(*,'(a)') '# SED sample  case=solar  columns=wavelength_AA  flux_erg_s_cm2_AA'
    do i = 1, n_wav, stride
-      write(*,'(f12.2, 1x, 1pe22.12)') wavelengths(i), fluxes(i)
+      write(*,'(1pe26.16, 1x, 1pe26.16)') wavelengths(i), fluxes(i)
    end do
 
    ! -----------------------------------------------------------------------
