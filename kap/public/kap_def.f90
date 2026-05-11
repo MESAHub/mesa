58b84b06d9271cac12a708b07e69c0d47bd66ae5
Emily Sandford <emily.sandford815@gmail.com>
2026-05-11 23:36:35 +0200

    fixed incorrect metal mass fraction array for Freedman lowT opacity tables (#993)
    Co-authored-by: Emily Sandford <emily@Emilys-MacBook-Air.local>
    
    Louis Siebenaler (cc'd) and I noticed a problem with the preprocessing & evaluation of the Freedman11 low-temperature opacity tables. Specifically, the "Z" values associated with each table, as e.g. enumerated in $MESA_DIR/kap/public/kap_def.f90,
    num_kap_lowT_Zs(i) = 7
    kap_lowT_Zs(1:num_kap_lowT_Zs(i), i) = [ 0.01d0, 0.02d0, 0.04d0, 0.100d0, 0.200d0, 0.63d0, 1.00d0 ]
    
    are not metal mass fractions. Rather, they are related to number fractions; for example, a Freedman [M/H] value of -0.3 corresponds to a metal number fraction of 10**(-0.3)solar = 0.5solar; [M/H]=0 corresponds to a solar number fraction, etc. It appears that there was a decision made to equate the solar metal number fraction with a metal mass fraction of 0.02 and scale the other values in the array accordingly.
    
    The metal mass fraction array should instead look something like this (with small variations possible depending on which protosolar abundance table you work with; the below values are based on Lodders 2003 table 2):
    [M/H] = [-0.3, 0.0, 0.3, 0.7, 1.0, 1.5, 1.7]
    Z = [0.0075, 0.0148, 0.029, 0.0698, 0.1303, 0.3215, 0.4288]

--- kap/public/kap_def.f90
+++ kap/public/kap_def.f90
@@ -618,7 +618,7 @@ contains
           kap_lowT_Xs(1:num_kap_lowT_Xs(i), i) = [ 0.00d0 ]
           num_kap_lowT_Zs(i) = 7
           kap_lowT_Zs(1:num_kap_lowT_Zs(i), i) = &
-             [ 0.01d0, 0.02d0, 0.04d0, 0.100d0, 0.200d0, 0.63d0, 1.00d0 ]
+             [ 0.007d0, 0.015d0, 0.029d0, 0.07d0,  0.13d0,  0.321d0, 0.429d0 ]
           num_kap_lowT_Xs_for_this_Z(1:num_kap_lowT_Zs(i), i) = num_kap_lowT_Xs(i)
        case DEFAULT
           num_kap_lowT_Xs(i) = 10
