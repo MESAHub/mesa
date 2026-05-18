5987df48c2d083bf21f0397853b1ca21b2f0524c
Debraheem <ekfarag@asu.edu>
2026-05-17 20:40:38 -0400

    run from inlist_binary instead of inlist

--- star/dev_cases_massive_stars/15M_binary_stripped_envelope/src/binary_run.f90
+++ star/dev_cases_massive_stars/15M_binary_stripped_envelope/src/binary_run.f90
@@ -1,6 +1,11 @@
       program binary_run
-      use run_binary, only: do_run_binary
-      
-      call do_run_binary(.true.)
+      use binary_lib, only: run1_binary
+      use run_star_extras
+      use run_binary_extras
+
+      integer :: ierr
+
+      call run1_binary(.true., extras_controls, extras_binary_controls, &
+         ierr, 'inlist_binary')
       
       end program
