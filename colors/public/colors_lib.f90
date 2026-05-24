bf737a0b64b5101ce1d2af3f5e39625f54c5dd3c
Niall Miller <niall.j.miller@gmail.com>
2026-05-24 16:41:44 -0600

    Remove commented mesa test watchdog check

--- colors/public/colors_lib.f90
+++ colors/public/colors_lib.f90
@@ -14,7 +14,7 @@
 !
 !   You should have received a copy of the GNU Lesser General Public License
 !   along with this program. If not, see <https://www.gnu.org/licenses/>.
-!   mesa test watchdog check retry
+!   
 ! ***********************************************************************
 
 module colors_lib
