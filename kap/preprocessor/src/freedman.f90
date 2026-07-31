MESAHub/mesa

commit 0030af617f43c3edd8a97b2fda967e6469d4d934

kap: port preprocessor to new build system
Some of the preprocessor code has been modified to actually build, and
to catch some errors in a cleaner way

kap/preprocessor/Makefile
@@ -1,0 +1,147 @@
+include ../../make/defaults-module.mk
+
+MODULE_NAME := kap-preprocessor
+SRCS := src/co_enhanced.f90 \
+	src/create_tables.f90 \
+	src/ferg_logP.f90 \
+	src/ferguson.f90 \
+	src/fixed_metal.f90 \
+	src/freedman.f90 \
+	src/kap_support.f90 \
+	src/opal_type1.f \
+	src/opal_type2.f
+SRCS_CHECK :=
+INTERNAL_DEPENDS_ON := kap
+EXTERNAL_DEPENDS_ON :=
+CHECK_RESULTS_GOLDEN :=
+INCLUDE_DIRS := -Isrc
+
+# Install
+
+MODULES :=
+INSTALL_INCLUDES :=
+BINTYPE := executable
+
+include ../../make/Makefile
+
+$(BUILD_DIR_MODULE)/kap_input_data/: kap_input_data.tar.xz
+	tar -xf kap_input_data.tar.xz -C $(BUILD_DIR_MODULE)
+
+$(BUILD_DIR_MODULE)/ags09/: AESOPUS/aesopus_ags09.tar.xz
+	mkdir -p $@
+	tar -xf AESOPUS/aesopus_ags09.tar.xz -C $@
+
+$(BUILD_DIR_MODULE)/logT_points_Fe $(BUILD_DIR_MODULE)/logT_points_highT_8.0 $(BUILD_DIR_MODULE)/logT_points_highT_8.7 $(BUILD_DIR_MODULE)/logT_points_lowT_2.7 $(BUILD_DIR_MODULE)/logT_points_lowT_3.0 $(BUILD_DIR_MODULE)/logT_points_lowT_Freedman11:
+	cp $(notdir $@) $@
+
+logT_points: $(BUILD_DIR_MODULE)/logT_points_Fe $(BUILD_DIR_MODULE)/logT_points_highT_8.0 $(BUILD_DIR_MODULE)/logT_points_highT_8.7 $(BUILD_DIR_MODULE)/logT_points_lowT_2.7 $(BUILD_DIR_MODULE)/logT_points_lowT_3.0 $(BUILD_DIR_MODULE)/logT_points_lowT_Freedman11
+
+
+define kap
+@echo kap-preprocessor $1 $2 $3
+@cd $(BUILD_DIR_MODULE); $(OBJ_OUT) $(CURDIR)/$1 $2 $3
+endef
+
+define kap-fixed-metal
+$(call kap,$1,0.0000,-1)
+$(call kap,$1,0.0001,-1)
+$(call kap,$1,0.0003,-1)
+$(call kap,$1,0.0010,-1)
+$(call kap,$1,0.0020,-1)
+$(call kap,$1,0.0040,-1)
+$(call kap,$1,0.0100,-1)
+$(call kap,$1,0.0200,-1)
+$(call kap,$1,0.0300,-1)
+$(call kap,$1,0.0400,-1)
+$(call kap,$1,0.0600,-1)
+$(call kap,$1,0.0800,-1)
+$(call kap,$1,0.1000,-1)
+endef
+
+define kap-co-enhanced
+$(call kap,$1,0.001,-1)
+$(call kap,$1,0.004,-1)
+$(call kap,$1,0.010,-1)
+$(call kap,$1,0.020,-1)
+$(call kap,$1,0.030,-1)
+$(call kap,$1,0.050,-1)
+$(call kap,$1,0.100,-1)
+endef
+
+define kap-freedman
+$(call kap,$1,0.01,0)
+$(call kap,$1,0.02,0)
+$(call kap,$1,0.04,0)
+$(call kap,$1,0.10,0)
+$(call kap,$1,0.20,0)
+$(call kap,$1,0.63,0)
+$(call kap,$1,1.00,0)
+endef
+
+define kap-oplib
+python $(CURDIR)/spline_OPLIB_tables.py $(BUILD_DIR_MODULE)/kap_input_data/oplib/$1/ $(BUILD_DIR_MODULE)/data/kap_data/
+endef
+
+build-data: $(BUILD_DIR_MODULE)/kap_input_data/ logT_points $(OBJ_OUT) | $(BUILD_DIR_MODULE)/data/kap_data/
+	# As of r8118 file is empty kap_input_data/opal/A05+Ne
+	# do_one inlist_a05+Ne
+# This is broken (SIGFPE due to negative opacity), 1.0 is way outside the table
+# boundaries (also doesn't seem to be in use)
+#	$(call kap,inlist_Fe,1.0,0.0)
+
+	$(call kap-fixed-metal,inlist_a09)
+	$(call kap-co-enhanced,inlist_a09_CO)
+
+	$(call kap-fixed-metal,inlist_OP_a09_nans_removed_by_hand)
+	$(call kap-fixed-metal,inlist_OP_gs98)
+
+	$(call kap-freedman,inlist_lowT_Freedman11)
+
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98)
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98_aFe_m2)
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98_aFe_p2)
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98_aFe_p4)
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98_aFe_p6)
+	$(call kap-fixed-metal,inlist_lowT_fa05_gs98_aFe_p8)
+
+	$(call kap-fixed-metal,inlist_lowT_fa05_gn93)
+	$(call kap-fixed-metal,inlist_lowT_fa05_ags04)
+	$(call kap-fixed-metal,inlist_lowT_fa05_a09)
+
+	$(call kap-fixed-metal,inlist_lowT_af94_gn93)
+
+	$(call kap-fixed-metal,inlist_gn93)
+	$(call kap-co-enhanced,inlist_gn93_CO)
+
+	$(call kap-fixed-metal,inlist_gs98)
+	$(call kap-co-enhanced,inlist_gs98_CO)
+	$(call kap-fixed-metal,inlist_gs98_aFe_m2)
+	$(call kap-fixed-metal,inlist_gs98_aFe_p2)
+	$(call kap-fixed-metal,inlist_gs98_aFe_p3)
+	$(call kap-fixed-metal,inlist_gs98_aFe_p4)
+	$(call kap-co-enhanced,inlist_gs98_aFe_p4_CO)
+	$(call kap-fixed-metal,inlist_gs98_aFe_p6)
+	$(call kap-fixed-metal,inlist_gs98_aFe_p8)
+
+	$(call kap-oplib,OPLIB_GS98_LOG_1194_25E)
+	$(call kap-oplib,OPLIB_AGSS09_LOG_1194_25E)
+	$(call kap-oplib,OPLIB_AAG21_LOG_1194_25E)
+	$(call kap-oplib,OPLIB_MB22_LOG_1194_25E)
+
+	$(call kap-fixed-metal,inlist_lowT_fa05_mb22)
+
+	cp -r $(BUILD_DIR_MODULE)/kap_input_data/wichita/AAG21_lowT_MESA_tables/*  $(BUILD_DIR_MODULE)/data/kap_data/
+	cp condtable.data $(BUILD_DIR_MODULE)/data/kap_data/
+
+$(BUILD_DIR_MODULE)/AESOPUS_AGSS09.h5: $(BUILD_DIR_MODULE)/ags09/
+	cd $(BUILD_DIR_MODULE); python $(CURDIR)/AESOPUS/aesopus.py $(CURDIR)/AESOPUS/AGSS09.yaml
+
+build-aesopus: $(BUILD_DIR_MODULE)/AESOPUS_AGSS09.h5
+
+export-data:
+	rm -f ../kap_data.tar ../kap_data.tar.xz ../AESOPUS_AGSS09.h5
+	tar -cRf ../kap_data.tar -C $(BUILD_DIR_MODULE)/data kap_data
+	xz ../kap_data.tar
+	cp $(BUILD_DIR_MODULE)/AESOPUS_AGSS09.h5 ..
+
+.PHONY: build-data export-data

kap/i1_preprocessors
@@ -1,18 +1,0 @@
-#!/usr/bin/env bash
-
-function check_okay {
-	if [ $? -ne 0 ]
-	then
-		echo
-		pwd
-		echo "build FAILED"
-		echo
-		exit 1
-	fi
-}
-
-cd preprocessor/make
-make ../ckap
-check_okay
-cd ../..
-

kap/preprocessor/AESOPUS/aesopus.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 import h5py
-import io
 import os
 import sys
 import yaml
@@ -61,8 +60,11 @@ class AesopusTable:
     def _read_table(self, table):
         """Extract opacities into array"""
 
-        stream = io.StringIO("".join(table))
-        data = np.loadtxt(stream, comments="#", dtype="float32")
+        data = np.loadtxt(
+            (r.replace("-250.0000", " -99.0000") for r in table),
+            comments="#",
+            dtype="float32",
+        )
 
         # data has temperatures in decreasing order; reverse
         self.data = np.flipud(data[:, 1:])

kap/preprocessor/AESOPUS/build_4_export
@@ -1,8 +1,0 @@
-cp AESOPUS_AGSS09.h5 ../..
-
-echo "**************"
-echo "**************"
-echo "next step is"
-echo "cd ../.. ; ./build_data_and_export"
-echo "**************"
-echo "**************"

kap/preprocessor/AESOPUS/preprocess
@@ -1,18 +1,0 @@
-#!/usr/bin/env bash
-
-# unpack tables
-mkdir -p ags09
-tar xv -C ags09 -f aesopus_ags09.tar.xz 
-
-# many of these AESOPUS tables contain lines like
-# 3.200-250.0000-250.0000-250.0000...
-# since there is no space separation, the columns are not correctly detected
-# this replaces "-250.0000" with " -99.0000" to circumvent the issue
-
-for f in ags09/*.tab
-do
-    sed -i 's/-250.0000/ -99.0000/g' $f
-done
-
-# run python script
-./aesopus.py AGSS09.yml

kap/preprocessor/build_4_export
@@ -1,18 +1,0 @@
-# run this from kap/preprocessor to create kap_data.tar.xz in the kap directory
-rm -rf ../data/kap_data/cache
-cd data
-rm -rf kap_data.tar
-rm -rf kap_data.tar.xz
-cp ../condtabl.d kap_data/condtabl.data
-echo 'tar -cvf kap_data.tar kap_data'
-tar -cvf kap_data.tar kap_data/*.data
-echo 'xz kap_data.tar'
-xz kap_data.tar
-cp kap_data.tar.xz ../..
-
-echo "**************"
-echo "**************"
-echo "next step is"
-echo "cd .. ; ./build_data_and_export"
-echo "**************"
-echo "**************"

kap/preprocessor/build_kap_input_tar
@@ -1,3 +1,0 @@
-rm -rf kap_input_data.tar.xz kap_input_data.tar
-tar -cvf kap_input_data.tar kap_input_data
-xz kap_input_data.tar

kap/preprocessor/ck
@@ -1,2 +1,0 @@
-./../../utils/test/ck
-echo "NOTE: ck assumes ckap built with do_kappa_test = .true. in create_tables"

commit 5abf7c6f8d6a134e67c2a096b34357407b1d98f7

fixed incorrect metal mass fraction array for Freedman lowT opacity t… (#994)
* fixed incorrect metal mass fraction array for Freedman lowT opacity tables (#993)

Co-authored-by: Emily Sandford <emily@Emilys-MacBook-Air.local>

Louis Siebenaler (cc'd) and I noticed a problem with the preprocessing & evaluation of the Freedman11 low-temperature opacity tables. Specifically, the "Z" values associated with each table, as e.g. enumerated in $MESA_DIR/kap/public/kap_def.f90,
num_kap_lowT_Zs(i) = 7
kap_lowT_Zs(1:num_kap_lowT_Zs(i), i) = [ 0.01d0, 0.02d0, 0.04d0, 0.100d0, 0.200d0, 0.63d0, 1.00d0 ]

are not metal mass fractions. Rather, they are related to number fractions; for example, a Freedman [M/H] value of -0.3 corresponds to a metal number fraction of 10**(-0.3)solar = 0.5solar; [M/H]=0 corresponds to a solar number fraction, etc. It appears that there was a decision made to equate the solar metal number fraction with a metal mass fraction of 0.02 and scale the other values in the array accordingly.

The metal mass fraction array should instead look something like this (with small variations possible depending on which protosolar abundance table you work with; the below values are based on Lodders 2003 table 2):
[M/H] = [-0.3, 0.0, 0.3, 0.7, 1.0, 1.5, 1.7]
Z = [0.0075, 0.0148, 0.029, 0.0698, 0.1303, 0.3215, 0.4288]

* add more sig figs, regenerate kap_data using dev_requirments python version, add known_bugs/changelog docs, fix kap preprocessor typos and missing kap file build call introduced by 0030af617 ("kap: port preprocessor to new build system", update atm test output).

* [ci skip] update Freedman lowT opacity documentation

---------

Co-authored-by: Emily Sandford <emily.sandford815@gmail.com>

kap/AESOPUS_AGSS09.h5
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ec345f74715752d4884afc1fbcb824f2b29c1c483630b647183ba531ddc146d9
-size 35605321
+oid sha256:e6d0733d381ef38bd7a836bdf13ff082cda9d4332fd39aaf19443dc9448c3264
+size 35095968

kap/kap_data.tar.xz
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05d43caa14cadc43eedd4d26e5182acc416cae7434dd4ba8a0a55e8eedd5129c
-size 95406276
+oid sha256:e195100180a32c7bccf506c85d506b497ed78a5bb01abbfdadd96f6026054a65
+size 113517544

kap/preprocessor/Makefile
@@ -59,6 +59,7 @@ $(call kap,$1,0.1000,-1)
 endef
 
 define kap-co-enhanced
+$(call kap,$1,0.000,-1)
 $(call kap,$1,0.001,-1)
 $(call kap,$1,0.004,-1)
 $(call kap,$1,0.010,-1)
@@ -69,13 +70,13 @@ $(call kap,$1,0.100,-1)
 endef
 
 define kap-freedman
-$(call kap,$1,0.01,0)
-$(call kap,$1,0.02,0)
-$(call kap,$1,0.04,0)
-$(call kap,$1,0.10,0)
-$(call kap,$1,0.20,0)
-$(call kap,$1,0.63,0)
-$(call kap,$1,1.00,0)
+$(call kap,$1,0.007452,0)
+$(call kap,$1,0.014760,0)
+$(call kap,$1,0.029024,0)
+$(call kap,$1,0.069840,0)
+$(call kap,$1,0.130292,0)
+$(call kap,$1,0.321456,0)
+$(call kap,$1,0.428844,0)
 endef
 
 define kap-oplib
@@ -131,10 +132,10 @@ build-data: $(BUILD_DIR_MODULE)/kap_input_data/ logT_points $(OBJ_OUT) | $(BUILD
 	$(call kap-fixed-metal,inlist_lowT_fa05_mb22)
 
 	cp -r $(BUILD_DIR_MODULE)/kap_input_data/wichita/AAG21_lowT_MESA_tables/*  $(BUILD_DIR_MODULE)/data/kap_data/
-	cp condtable.data $(BUILD_DIR_MODULE)/data/kap_data/
+	cp condtabl.data $(BUILD_DIR_MODULE)/data/kap_data/
 
 $(BUILD_DIR_MODULE)/AESOPUS_AGSS09.h5: $(BUILD_DIR_MODULE)/ags09/
-	cd $(BUILD_DIR_MODULE); python $(CURDIR)/AESOPUS/aesopus.py $(CURDIR)/AESOPUS/AGSS09.yaml
+	cd $(BUILD_DIR_MODULE); python $(CURDIR)/AESOPUS/aesopus.py $(CURDIR)/AESOPUS/AGSS09.yml
 
 build-aesopus: $(BUILD_DIR_MODULE)/AESOPUS_AGSS09.h5
 

kap/preprocessor/build_data_and_export
@@ -1,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+kap_data_dir="../../build/kap-preprocessor/data/kap_data"
+
+rm -rf "$kap_data_dir"
+mkdir -p "$kap_data_dir"
+make build-data build-aesopus export-data

kap/preprocessor/src/freedman.f90
@@ -41,21 +41,21 @@ contains
       character(len=*), intent(in) :: data_dir
       character(len=*), intent(out) :: fname
       integer :: iz
-      iz = floor(Z*1d5 + 0.1d0)
+      iz = nint(Z*1d6)
       select case (iz)
-      case (1000)
+      case (7452)
          fname = trim(data_dir)//'/m0.3.txt'
-      case (2000)
+      case (14760)
          fname = trim(data_dir)//'/p0.0.txt'
-      case (4000)
+      case (29024)
          fname = trim(data_dir)//'/p0.3.txt'
-      case (10000)
+      case (69840)
          fname = trim(data_dir)//'/p0.7.txt'
-      case (20000)
+      case (130292)
          fname = trim(data_dir)//'/p1.0.txt'
-      case (63000)
+      case (321456)
          fname = trim(data_dir)//'/p1.5.txt'
-      case (100000)
+      case (428844)
          fname = trim(data_dir)//'/p1.7.txt'
       case default
          write (*, *) 'get_Freedman_fname: unexpected Z value for Freedman data', Z

kap/public/kap_def.f90
@@ -618,7 +618,8 @@ contains
           kap_lowT_Xs(1:num_kap_lowT_Xs(i), i) = [ 0.00d0 ]
           num_kap_lowT_Zs(i) = 7
           kap_lowT_Zs(1:num_kap_lowT_Zs(i), i) = &
-             [ 0.01d0, 0.02d0, 0.04d0, 0.100d0, 0.200d0, 0.63d0, 1.00d0 ]
+             [ 0.007452d0, 0.014760d0, 0.029024d0, 0.069840d0, &
+               0.130292d0, 0.321456d0, 0.428844d0 ]
           num_kap_lowT_Xs_for_this_Z(1:num_kap_lowT_Zs(i), i) = num_kap_lowT_Xs(i)
        case DEFAULT
           num_kap_lowT_Xs(i) = 10

atm/test/test_output
@@ -85,17 +85,17 @@
 
  test_irradiated
 
-                              test_grey_irradiated: kap    4.3649748642304882D-03
+                              test_grey_irradiated: kap    4.7498368201485917D-03
                                             M/M_jupiter    1.4999999999999998D+00
                                             R/R_jupiter    1.5743735095633800D+00
                                                  R/Rsun    1.6178684913857289D-01
                                                   kap_v    4.0000000000000001D-03
                                                       P    1.0000000000000000D+06
 
-                                                    tau    2.9101126947637423D+00
+                                                    tau    3.1669763036977079D+00
                                                    Teff    5.7759999999999891D+03
                                                    T_eq    1.0000000000000000D+03
                                                   T_int    9.0000000000000000D+02
-                                                      T    1.2929349604416070D+03
-                                                   logT    3.1115766787614829D+00
+                                                      T    1.3124222808279656D+03
+                                                   logT    3.1180735947393834D+00
 