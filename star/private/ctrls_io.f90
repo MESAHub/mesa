6021662879030441da6926b9849a2626a6a3260d
Evan Bauer <18405113+evbauer@users.noreply.github.com>
2026-08-20 17:40:34 -0400

    Add three component phase separation for ultramassive white dwarfs (#863)
    Note: WD phase separation smooth heating is enabled default.
    
    The rate of phase separation heating is somewhat mesh and timestep
    dependent. It should get the integrated amount of energy right, but
    we are not resolving the spatial and time scales over which it occurs.
    This led to an undesirable behavior at small time steps where
    crystallizing a zone could cause enough mixing to generate a transient
    heating spike. When localized near the crystallization front, this
    could re-melt previously crystallized material in an unphysical
    way. The smoothing approach here redistributes this energy throughout
    the inner half of the star, which should prevent re-melting, so we at
    least get the right amount of energy into the stellar interior, while
    we might not be precisely locating it in the right place.
    This changes the default behavior somewhat, so I've included an
    inlist flag in case somebody wants to turn this off to reproduce
    previously published behavior.
    
    ---------
    
    Co-authored-by: mcastrotapia <113072908+mcastrotapia@users.noreply.github.com>
    Co-authored-by: evbauer <bauer39@llnl.gov>
    Co-authored-by: pmocz <philip.mocz@gmail.com>

--- star/private/ctrls_io.f90
+++ star/private/ctrls_io.f90
@@ -327,6 +327,7 @@
     do_phase_separation, &
     phase_separation_option, &
     do_phase_separation_heating, &
+    smooth_phase_separation_heating, &
     phase_separation_mixing_use_brunt, &
     phase_separation_no_diffusion, &
 
@@ -1779,6 +1780,7 @@ s% gradT_excess_max_log_tau_full_off = gradT_excess_max_log_tau_full_off
  s% do_phase_separation = do_phase_separation
  s% phase_separation_option = phase_separation_option
  s% do_phase_separation_heating = do_phase_separation_heating
+ s% smooth_phase_separation_heating = smooth_phase_separation_heating
  s% phase_separation_mixing_use_brunt = phase_separation_mixing_use_brunt
  s% phase_separation_no_diffusion = phase_separation_no_diffusion
 
@@ -3442,6 +3444,7 @@ s% gradT_excess_max_log_tau_full_off = gradT_excess_max_log_tau_full_off
  do_phase_separation = s% do_phase_separation
  phase_separation_option = s% phase_separation_option
  do_phase_separation_heating = s% do_phase_separation_heating
+ smooth_phase_separation_heating = s% smooth_phase_separation_heating
  phase_separation_mixing_use_brunt = s% phase_separation_mixing_use_brunt
  phase_separation_no_diffusion = s% phase_separation_no_diffusion
 
---
+++ star/test_suite/wd_o_ne_3_phase/wd1_10_mi8_3_NeOMg.mod
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:123e893958689c6803f62b68fc657fa247a53e1b4430747776ab12bd64797cdc
+size 565419
